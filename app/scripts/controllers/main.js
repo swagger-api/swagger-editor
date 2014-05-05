71694c76e503f5cee434f62358d9b17cbf9ebf8a
Mohsen Azimi <msn.azi@gmail.com>
2014-05-05 12:39:52 -0700

    jshint fix

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,20 +10,21 @@ angular.module('koknusApp')
     };
 
     $scope.aceChanged = function(editor) {
-      // TODO
+      console.log(editor);
     };
 
     $scope.switchToLanguage = function(language){
+      /* global jsyaml */
       $scope.editingLanguage = language;
       var currentValue = $scope.editor.getSession().getValue();
       var newValue = null;
-      if(language == 'yml'){
+      if(language === 'yml'){
         newValue = JSON.parse(currentValue);
         newValue = jsyaml.dump(newValue);
       }
-      if(language == 'json'){
+      if(language === 'json'){
         newValue = jsyaml.load(currentValue);
-        newValue = JSON.stringify(newValue, null, 2)
+        newValue = JSON.stringify(newValue, null, 2);
       }
       $scope.editor.getSession().setValue(newValue);
     };
