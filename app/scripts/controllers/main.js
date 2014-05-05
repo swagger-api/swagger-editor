58f2053a10db103e45c20dff7f21019ee69c6481
Mohsen Azimi <msn.azi@gmail.com>
2014-05-05 15:05:50 -0700

    Yaml to Json error handler

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,33 +1,60 @@
 'use strict';
+/* global jsyaml */
+
+function annotateErrors(editor){
+  var errorMessage = null;
+  var value = editor.getSession().getValue();
+  var json = null;
+  try {
+    json = jsyaml.load(value);
+  } catch(yamlLoadError) {
+    errorMessage = yamlLoadError.message.replace('JS-YAML: ', '');
+    editor.getSession().setAnnotations([{
+      row: yamlLoadError.mark.line,
+      column: yamlLoadError.mark.column,
+      text: errorMessage,
+      type: 'error'
+    }]);
+    return errorMessage;
+  }
+  editor.getSession().clearAnnotations();
+  return errorMessage;
+}
 
 PhonicsApp.controller('MainCtrl', function ($scope) {
-    $scope.editor = null;
-    $scope.editingLanguage = 'yml';
+  $scope.editor = null;
+  $scope.editingLanguage = 'yml';
+  $scope.editorErrorMessage = '';
 
-    $scope.aceLoaded = function(editor) {
-      $scope.editor = editor;
-    };
+  $scope.aceLoaded = function(editor) {
+    $scope.editor = editor;
+  };
 
-    $scope.aceChanged = function(editor) {
-      console.log(editor);
-    };
+  $scope.aceChanged = function() {
+    var error = annotateErrors($scope.editor);
+    if(!error) {
+      $scope.editorErrorMessage = '';
+    }
+  };
 
-    $scope.switchToLanguage = function(language){
-      /* global jsyaml */
-      $scope.editingLanguage = language;
-      var currentValue = $scope.editor.getSession().getValue();
-      var newValue = null;
-      if(language === 'yml'){
-        newValue = JSON.parse(currentValue);
-        newValue = jsyaml.dump(newValue);
-      }
-      if(language === 'json'){
-        newValue = jsyaml.load(currentValue);
-        newValue = JSON.stringify(newValue, null, 2);
-      }
-      $scope.editor.getSession().setValue(newValue);
-    };
+  $scope.switchToLanguage = function(language){
+    var currentValue = $scope.editor.getSession().getValue();
+    var newValue = null;
+    if(language === 'yml'){
+      newValue = JSON.parse(currentValue);
+      newValue = jsyaml.dump(newValue);
+    }
+    if(language === 'json'){
+      $scope.editorErrorMessage = annotateErrors($scope.editor);
+      newValue = JSON.stringify(newValue, null, 2);
+    }
+    if($scope.editorErrorMessage) {
+      return;
+    }
+    $scope.editingLanguage = language;
+    $scope.editor.getSession().setValue(newValue);
+  };
 
-  });
+});
 
 
