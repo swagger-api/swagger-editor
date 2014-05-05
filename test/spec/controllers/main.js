7cfc932d93234cafacd0b8abcf092c1835d59622
Mohsen Azimi <msn.azi@gmail.com>
2014-05-05 14:04:41 -0700

    Rename app

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -3,7 +3,7 @@
 describe('Controller: MainCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('koknusApp'));
+  beforeEach(module('PhonicsApp'));
 
   var MainCtrl,
     scope;
