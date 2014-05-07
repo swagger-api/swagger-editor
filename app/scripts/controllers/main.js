6fa4601f61508d044a4fd12f95e8b758d2a55449
Mohsen Azimi <msn.azi@gmail.com>
2014-05-06 19:22:57 -0700

    Put directives into thir own folder

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -119,32 +119,6 @@ PhonicsApp.controller('MainCtrl', ['$scope', function ($scope) {
     $scope.editor.getSession().setValue(newValue);
   };
 
-}]).directive('splitterBar', function(){
-  return {
-    template: '',
-    replace: false,
-    restrict: 'E',
-    link: function($scope, $element, $attributes){
-      var $document = $(document);
-      function resize(mouseMoveEvent){
-        var x = mouseMoveEvent.pageX;
-        var MIN_WIDTH = 100;
-        if( x < MIN_WIDTH || x > window.innerWidth - MIN_WIDTH) {
-          return;
-        }
-        $element.css('left', x);
-        $('#' + $attributes.leftPane).css('width', x);
-        $('#' + $attributes.rightPane).css('width',
-          window.innerWidth - x - $element.width());
-      }
-      $element.on('mousedown', function(){
-        $document.on('mousemove', resize);
-        $document.on('mouseup', function(){
-          $document.off('mousemove', resize);
-        });
-      });
-    }
-  };
-});
+}]);
 
 
