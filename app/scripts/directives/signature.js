c3da73780386da8daad2b8a9ce9599fb3d62176a
Mohsen Azimi <msn.azi@gmail.com>
2014-05-13 16:38:34 -0700

    Add sampleJSON

--- app/scripts/directives/signature.js
+++ app/scripts/directives/signature.js
@@ -1,7 +1,23 @@
 'use strict';
 
 PhonicsApp.directive('signature', function(){
+  function getSampleJSON(type, models) {
+    return SwaggerOperation.prototype.getSampleJSON(type, models);
+  };
+
+  function link(scope, element, attributes){
+    scope.getSampleJSON = getSampleJSON;
+    scope.visibilePane = 0;
+    scope.sampleJSON = getSampleJSON(scope.operation.type, scope.models);
+    scope.signature = 'stringify';
+  };
+
   return {
+    scope: {
+      operation: '=',
+      models: '='
+    },
+    link: link,
     restrict: 'E',
     replace: true,
     templateUrl: '/templates/signature.html'
