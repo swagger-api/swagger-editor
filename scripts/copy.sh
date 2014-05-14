345f0d3630193271dc06769da09c4af1eb2b7e56
Mohsen Azimi <msn.azi@gmail.com>
2014-05-14 15:51:13 -0700

    Add searchbox ace extension to copy.sh

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,1 +1,2 @@
 cp 'app/bower_components/ace-builds/src-min-noconflict/worker-json.js' 'dist/worker-json.js'
+cp 'app/bower_components/ace-builds/src-min-noconflict/ext-searchbox.js' 'dist/ext-searchbox.js'
