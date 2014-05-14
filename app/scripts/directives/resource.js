d8f85bf1970e626dcd54702c4ec90910606e0cba
Mohsen Azimi <msn.azi@gmail.com>
2014-05-14 16:57:33 -0700

    Make endpoints and apis collapsable

--- app/scripts/directives/resource.js
+++ app/scripts/directives/resource.js
@@ -4,6 +4,19 @@ PhonicsApp.directive('resource', function(){
   return {
     restrict: 'E',
     replace: true,
-    templateUrl: 'templates/resource.html'
+    templateUrl: 'templates/resource.html',
+    link: function(scope, element){
+      scope.show = true;
+
+      scope.collapseAll = function(){
+        scope.show = true;
+        $('.content', element).addClass('hidden');
+      };
+
+      scope.expandAll = function(){
+        scope.show = true;
+        $('.content', element).removeClass('hidden');
+      };
+    }
   };
 });
