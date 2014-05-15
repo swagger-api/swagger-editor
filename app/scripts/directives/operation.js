0c93e14efe2d1125f5f72d5b1f315b984756a2e2
Mohsen Azimi <msn.azi@gmail.com>
2014-05-15 16:59:49 -0700

    Add validation for Try It function

--- app/scripts/directives/operation.js
+++ app/scripts/directives/operation.js
@@ -1,9 +1,41 @@
 'use strict';
 
-PhonicsApp.directive('operation', function(){
+PhonicsApp.directive('operation', ['$timeout', function($timeout){
+  function requiredFieldsAreFilled (operation){
+    var allRequiredsFilled = true;
+    operation.parameters.forEach(function(param){
+      if(param.required && !param.inputValue){
+        allRequiredsFilled = false;
+        param.shake =  true;
+        $timeout(function(){ param.shake = false; }, 900);
+      }
+    });
+    return allRequiredsFilled;
+  }
+
+  function tryIt(operation){
+    if(!requiredFieldsAreFilled(operation)) {
+      return;
+    }
+
+    var data = operation.parameters.map(function (param){
+      var o = Object.create(null);
+      o[param.name] = param.inputValue;
+      return o;
+    });
+    $.ajax({
+      method: operation.method,
+      data: data
+    }).then(function(result){
+      console.log(result);
+    });
+  }
   return {
     restrict: 'E',
     replace: true,
-    templateUrl: 'templates/operation.html'
+    templateUrl: 'templates/operation.html',
+    link: function(scope){
+      scope.tryIt = tryIt;
+    }
   };
-});
+}]);
