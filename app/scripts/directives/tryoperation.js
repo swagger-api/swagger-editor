b4d07866c133d0acc03d9e0b77940d5c660670b0
Mohsen Azimi <mazimi@apigee.com>
2014-07-31 11:48:50 -0700

    Introduce Content-Type header
    Bug: ng-init in ng-select is not working

--- app/scripts/directives/tryoperation.js
+++ app/scripts/directives/tryoperation.js
@@ -10,12 +10,26 @@ PhonicsApp.directive('tryOperation', function () {
     },
     link: function postLink(scope) {
       scope.httpProtorcol = 'HTTP/1.1';
-      scope.paramModels = {};
+      scope.paramModels = {
+        // '__content_type__': scope.getContentTypeHeaders()[0]
+      };
       scope.hasBody = scope.$parent.operation.parameters.some(function (parameter) {
         return parameter.in === 'body';
       });
+      scope.generateUrl = generateUrl;
+      scope.makeCall = makeCall;
+      scope.getContentTypeHeaders = getContentTypeHeaders;
 
-      scope.generateUrl = function  () {
+
+      function getContentTypeHeaders() {
+        if (scope.$parent.operation.consumes) {
+          return scope.$parent.operation.consumes;
+        } else {
+          return scope.$parent.$parent.$parent.$parent.consumes;
+        }
+      }
+
+      function generateUrl () {
 
         // FIXME: fix this insanity!
         var host = scope.$parent.$parent.$parent.$parent.host;
@@ -37,11 +51,11 @@ PhonicsApp.directive('tryOperation', function () {
 
 
         return host + pathTemplate(pathParams) + (queryParamsStr ? '?' + queryParamsStr : '');
-      };
+      }
 
-      scope.makeCall = function () {
+      function makeCall() {
         window.alert('TODO');
-      };
+      }
     }
   };
 });
