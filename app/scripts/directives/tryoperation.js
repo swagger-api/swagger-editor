3e9102063cb60564e523a2d496864e326303ea3a
Mohsen Azimi <mazimi@apigee.com>
2014-07-30 18:17:14 -0700

    Support path parameters'

--- app/scripts/directives/tryoperation.js
+++ app/scripts/directives/tryoperation.js
@@ -19,8 +19,17 @@ PhonicsApp.directive('tryOperation', function () {
 
         // FIXME: fix this insanity!
         var host = scope.$parent.$parent.$parent.$parent.host;
+        var path = scope.$parent.$parent.pathName;
+        var pathTemplate = _.template(path);
+        var pathParams = scope.$parent.operation.parameters.reduce(function (pathParams, parameter) {
+          if (parameter.in === 'path') {
+            pathParams[parameter.name] = scope.paramModels[parameter.name];
+          }
+          return pathParams;
+        }, {});
 
-        return host + scope.$parent.$parent.pathName;
+
+        return host + pathTemplate(pathParams);
       };
 
       scope.makeCall = function () {
