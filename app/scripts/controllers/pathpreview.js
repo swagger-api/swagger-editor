e8596e3fcd18db217d60e612a93a8ebe0a0aeb19
Mohsen Azimi <mazimi@apigee.com>
2014-08-13 11:47:34 -0700

    WIP: routing

---
+++ app/scripts/controllers/pathpreview.js
@@ -0,0 +1,10 @@
+'use strict';
+
+PhonicsApp.controller('PathPreviewCtrl', function ($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Karma'
+  ];
+  console.log('PathPreviewCtrl');
+});
--- app/scripts/router.js
+++ app/scripts/router.js
@@ -12,7 +12,7 @@ function Router($compileProvider, $stateProvider, $urlRouterProvider) {
 
   $stateProvider
   .state('home', {
-    url: '/',
+    url: '',
     views: {
       '': {
         templateUrl: 'views/main.html',
@@ -22,19 +22,34 @@ function Router($compileProvider, $stateProvider, $urlRouterProvider) {
         templateUrl: 'views/header/header.html',
         controller: 'HeaderCtrl'
       },
-      'editor@home': { templateUrl: 'views/editor/editor.html' },
-      'preview@home': { templateUrl: 'views/preview/preview.html' }
-    }
-  })
-  .state('home.operation', {
-    url: ':operationId',
-    views: {
+      'editor@home': {
+        templateUrl: 'views/editor/editor.html',
+        controller: 'EditorCtrl'
+      },
       'preview@home': {
-        controller: 'PreviewCtrl',
-        templateUrl: 'views/preview/preview.html'
+        templateUrl: 'views/preview/preview.html',
+        controller: 'PreviewCtrl'
       }
     }
-  });
+  })
+    .state('home.path', {
+      url: '/paths/:pathId',
+      views: {
+        'preview@home.path': {
+          templateUrl: 'views/preview/preview.html',
+          controller: 'PathPreviewCtrl'
+        }
+      }
+    });
+      // .state('home.path.operation', {
+      //   url: ':operationId',
+      //   views: {
+      //     'preview@home.path.operation': {
+      //       controller: 'PreviewCtrl',
+      //       templateUrl: 'views/preview/preview.html'
+      //     }
+      //   }
+      // });
 
 
   $compileProvider.aHrefSanitizationWhitelist('.');
