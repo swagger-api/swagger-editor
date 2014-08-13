1e21c6e999fa803249b0c0f7ac00c000874e7bac
Mohsen Azimi <mazimi@apigee.com>
2014-08-12 17:10:41 -0700

    Do not mangelify

--- Gruntfile.js
+++ Gruntfile.js
@@ -362,15 +362,18 @@ module.exports = function (grunt) {
     //     }
     //   }
     // },
-    // uglify: {
-    //   dist: {
-    //     files: {
-    //       '<%= yeoman.dist %>/scripts/scripts.js': [
-    //         '<%= yeoman.dist %>/scripts/scripts.js'
-    //       ]
-    //     }
-    //   }
-    // },
+    uglify: {
+      dist: {
+        options: {
+          mangle: false
+        },
+        files: {
+          '<%= yeoman.dist %>/scripts/scripts.js': [
+            '<%= yeoman.dist %>/scripts/scripts.js'
+          ]
+        }
+      }
+    },
     // concat: {
     //   dist: {}
     // },
