0b37597f73d18b553cb88a2589d90cffdb54309e
Mohsen Azimi <mazimi@apigee.com>
2014-08-18 10:00:49 -0700

    Fix build to NPM script

--- scripts/publish-npm.sh
+++ scripts/publish-npm.sh
@@ -1,6 +1,8 @@
 rm dist/package.json
-cp scripts/npm.js dist/package.json
+cp scripts/npm.json dist/package.json
+cd dist
 npm version patch
 npm publish
+cd ..
 rm scripts/npm.js
-cp dist/package.json scripts/npm.js
+cp dist/package.json scripts/npm.json
