1a0331b0e50067f60f55a822d181d01a844e6ecc
Mohsen Azimi <mazimi@apigee.com>
2014-09-02 20:52:28 -0500

    Update file loader to not build files while loading

--- app/scripts/services/fileloader.js
+++ app/scripts/services/fileloader.js
@@ -1,26 +1,42 @@
 'use strict';
 
-function load(fileContent) {
+/*
+** takes a JSON or YAML string, returns YAML string
+*/
+function load(string) {
+  var jsonError, yamlError;
 
-  // Figure out file type
-  var json = null;
-  var yaml = null;
+  if (!angular.isString(string)) {
+    throw new Error('load function only accepts a string');
+  }
+
+  // Try parsing the string as a YAML string  and capture the error
   try {
-    json = JSON.parse(fileContent);
-  } catch (jsonError) {}
-  if (!json) {
-    try {
-      yaml = jsyaml.load(fileContent);
-    } catch (yamlError) {}
+    jsyaml.load(string);
+  } catch (error) {
+    yamlError = error;
   }
 
-  if (json) {
-    return json;
+  // If there was no error in parsing the string as a YAML string
+  // return the original string
+  if (!yamlError) {
+    return string;
   }
-  if (typeof yaml === 'object') {
-    return yaml;
+
+  // If it wasn't a YAML string, try figuring out if it's a JSON string
+  try {
+    JSON.parse(string);
+  } catch (error) {
+    jsonError = error;
   }
-  return null;
+
+  // if it's a JSON string, convert it to YAML string and return it
+  if (!jsonError) {
+    return jsyaml.dump(JSON.parse(string));
+  }
+
+  // If it was neither JSON or YAML, throw an error
+  throw new Error('load function called with an invalid string');
 }
 
 PhonicsApp.service('FileLoader', function FileLoader($http) {
