8e8d4d6928eae8d17c42a4debe0028d09af84c53
Mohsen Azimi <mazimi@apigee.com>
2014-09-03 20:02:33 -0700

    Fix Squash script

--- scripts/squash.sh
+++ scripts/squash.sh
@@ -2,10 +2,7 @@
 
 #Squash NPM commits in one commits
 
-#Get the version number
-$VERSION = "git log -1 --pretty='format:%B'"
-
 # grunt ship makes 5 commits
 git reset --soft HEAD~5
 
-git commit -m "Update package versions to $VERSION"
+git commit -m "Update package versions"
