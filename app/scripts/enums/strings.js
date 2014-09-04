247184b24bc09eff0e141195cad7348e19ea2a5b
MMux <michaeljmoss@gmail.com>
2014-09-04 15:54:28 -0700

    change status msg, move to left

--- app/scripts/enums/strings.js
+++ app/scripts/enums/strings.js
@@ -3,10 +3,10 @@
 PhonicsApp.config(['$provide', function ($provide) {
   $provide.constant('strings', {
     stausMessages: {
-      '-2': 'Lost connection',
+      '-2': 'Unsaved Changes. Check your server connection',
       '-1': 'Error!',
       0: 'Working...',
-      1: 'Saved.'
+      1: 'All changes saved.'
     }
   });
 }]);
