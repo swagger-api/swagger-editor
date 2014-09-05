40d57b687579848e6dad983793763a49387d4a1c
Mohsen Azimi <mazimi@apigee.com>
2014-09-05 15:34:41 -0700

    Fix typo in A127 config

--- app/scripts/enums/apigee-127.js
+++ app/scripts/enums/apigee-127.js
@@ -10,7 +10,7 @@ PhonicsApp.config(['$provide', function ($provide) {
       servers: 'http://generator.wordnik.com/online/api/gen/servers',
       clients: 'http://generator.wordnik.com/online/api/gen/clients',
       server: 'http://generator.wordnik.com/online/api/gen/servers/{language}',
-      client: 'http://generator.wordnik.com/online/api/gen/client/{language}'
+      client: 'http://generator.wordnik.com/online/api/gen/clients/{language}'
     },
     exampleFiles: ['default.yaml', 'minimal.yaml', 'petstore.yaml', 'heroku-pets.yaml', 'uber.yaml'],
     backendEndpoint: '/editor/spec',
