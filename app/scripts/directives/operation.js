70d2c7123df20145638fc6157f2506ec57d32912
Mohsen Azimi <mazimi@apigee.com>
2014-10-08 14:51:36 -0700

    Bring back try operation

--- app/scripts/directives/operation.js
+++ app/scripts/directives/operation.js
@@ -5,6 +5,12 @@ PhonicsApp.directive('operation', [function () {
     restrict: 'E',
     replace: true,
     templateUrl: 'templates/operation.html',
-    scope: false
+    scope: false,
+    link: function ($scope) {
+      $scope.isTryOpen = false;
+      $scope.toggleTry = function toggleTry() {
+        $scope.isTryOpen = !$scope.isTryOpen;
+      };
+    }
   };
 }]);
