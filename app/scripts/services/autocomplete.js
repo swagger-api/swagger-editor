e0fb0cee194a6771e2ff7f0241ac52828f9e1c0d
Mohsen Azimi <mohsen@Moshen-Nabiloo-Azimis-MacBook-Pro.local>
2014-10-16 16:13:38 -0700

    Make snippet recommendation logic use current position

--- app/scripts/services/autocomplete.js
+++ app/scripts/services/autocomplete.js
@@ -1,32 +1,71 @@
 'use strict';
 
-PhonicsApp.service('Autocomplete', function Autocomplete(snippets) {
+PhonicsApp.service('Autocomplete', function Autocomplete(snippets, ASTManager) {
   var langTools = ace.require('ace/ext/language_tools');
   var snippetManager = ace.require('ace/snippets').snippetManager;
   var editor = null;
 
+
+  /*
+   * Check if a path is match with
+   * @param {array} path - path
+   * @param {array} matcher - matcher
+   * @returns {boolean} - true if it's match
+  */
+  function isMatchPath (path, matcher) {
+    if (!Array.isArray(path) || !Array.isArray(matcher)) {
+      return false;
+    }
+
+    if (path.length !== matcher.length) {
+      return false;
+    }
+
+    for (var i = 0; i < path.length; i++) {
+      if (path[i] !== matcher[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function filterForSnippets (pos) {
+    ASTManager.refresh(editor.getValue());
+    var path = ASTManager.pathForLine(pos.row);
+
+    // If there is no path being returned by AST Manager and only one character
+    // was typed, path is root
+    if (!path && pos.column === 1) {
+      path = [];
+    }
+
+    return function filter (snippet) {
+      return isMatchPath(path, snippet.path);
+    };
+  }
+
   var ASTCompleter = {
     getCompletions: function (editor, session, pos, prefix, callback) {
-      var showSnippets = true;
 
       editor.completer.autoSelect = true;
-      editor.completer.autoInsert = true;
 
-      if (showSnippets) {
-        langTools.snippetCompleter.getCompletions
-          .apply(langTools.snippetCompleter.getCompletions, arguments);
-      }
+      snippetManager.unregister(snippets, 'yaml');
+      snippetManager.register(snippets.filter(filterForSnippets(pos)), 'yaml');
 
+      langTools.snippetCompleter
+        .getCompletions(editor, session, pos, prefix, callback);
+
+
+      // Testing out
       callback(null, [
-        {name: 'flow', value: 'fooooe', score: 300, meta: 'swagger'},
-        {name: 'moor', value: 'loooos', score: 300, meta: 'swagger'}
+        // {name: 'flow', value: 'fooooe', score: 300, meta: 'swagger'},
+        // {name: 'moor', value: 'loooos', score: 300, meta: 'swagger'}
       ]);
     }
   };
 
   this.init = function (e) {
     editor = e;
-    snippetManager.register(snippets, 'yaml');
     editor.completers = [ASTCompleter];
   };
 });
