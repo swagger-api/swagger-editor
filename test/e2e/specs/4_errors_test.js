865e2eaf2c146301fb0f4eae02efe19b2ba7691d
Mohsen Azimi <msn.azi@gmail.com>
2014-10-21 17:32:15 -0700

    Error tests

--- test/e2e/specs/4_errors_test.js
+++ test/e2e/specs/4_errors_test.js
@@ -4,7 +4,24 @@
  * Test errors that is being presented to user
 */
 
-// describe('Errors', function () {
+function setValue (value) {
+  browser.executeScript(function (value) {
+    document.querySelector('[ui-ace]').env.editor.setValue(value);
+  }, value);
+}
 
+describe('Errors', function () {
 
-// });
+  it('should show no error on empty document', function () {
+    setValue('');
+    expect($('.error-presenter').isPresent()).toBe(false);
+  });
+
+  it('should show YAML syntax error with invalid YAML', function () {
+    setValue('invalid:1\n  yaml:');
+    browser.sleep(1200);
+    expect($('.error-presenter').isPresent()).toBe(true);
+    expect($('.error-header h4').getText()).toContain('YAML Syntax');
+  });
+
+});
