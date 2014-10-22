5aa2e78126395029ec90b87d6ca1caf6247777e4
Mohsen Azimi <msn.azi@gmail.com>
2014-10-21 17:36:10 -0700

    Swagger error test

--- test/e2e/specs/4_errors_test.js
+++ test/e2e/specs/4_errors_test.js
@@ -24,4 +24,23 @@ describe('Errors', function () {
     expect($('.error-header h4').getText()).toContain('YAML Syntax');
   });
 
+  it('should show Swagger Syntax Erorr with invalid swagger', function () {
+    var val = [
+      'swagger: 2.0',
+      'info:',
+      '  version: "1.0.0"',
+      '  title: Petstore',
+      'paths:',
+      '  /users:',
+      '    post:',
+      '      responses:',
+      '        200: {}'
+    ].join('\n');
+
+    setValue(val);
+    browser.sleep(1200);
+    expect($('.error-presenter').isPresent()).toBe(true);
+    expect($('.error-header h4').getText()).toContain('Swagger Error');
+  });
+
 });
