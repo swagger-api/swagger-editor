407f00d1a67b20269aae40cc738fed3108e79649
Mohsen Azimi <msn.azi@gmail.com>
2014-11-05 15:51:16 -0800

    use angular json filter

--- app/scripts/filters/get-resource-name-filter.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-PhonicsApp.filter('getResourceName', function () {
-  return function getResourceNameFilter(resource) {
-    return resource.resourcePath.replace(/\//g, '');
-  };
-});
