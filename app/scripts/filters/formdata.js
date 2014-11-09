162890e4831b96e8b6bdd7a108b20344d7ff86b8
Mohsen Azimi <msn.azi@gmail.com>
2014-11-09 15:56:49 -0800

    JSHintify and JSCSify formdata.js

--- app/scripts/filters/formdata.js
+++ app/scripts/filters/formdata.js
@@ -2,12 +2,13 @@
  * Renders a JavaScript object as form data
 */
 
+'use strict';
 
 PhonicsApp.filter('formdata', function () {
-  return function formdata (object) {
+  return function formdata(object) {
     var result = '';
 
-    if (angular.isObject(object)){
+    if (angular.isObject(object)) {
       Object.keys(object).forEach(function (key) {
 
         if (angular.isString(object[key])) {
@@ -17,5 +18,5 @@ PhonicsApp.filter('formdata', function () {
     }
 
     return result;
-  }
+  };
 });
