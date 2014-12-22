b9ed31eb8b981bb16fa993713d0f555af1c6c31b
Mohsen Azimi <msn.azi@gmail.com>
2014-12-22 13:44:10 -0800

    Use TapReporter in protractor

--- test/e2e/config.js
+++ test/e2e/config.js
@@ -10,6 +10,14 @@ var config = {
     }
   },
 
+  onPrepare: function() {
+    // The require statement must be down here, since jasmine-reporters
+    // needs jasmine to be in the global and protractor does not guarantee
+    // this until inside the onPrepare function.
+    require('jasmine-reporters');
+    jasmine.getEnv().addReporter(new jasmine.TapReporter());
+  },
+
   specs: ['specs/**/*test.js'],
 
   jasmineNodeOpts: {
