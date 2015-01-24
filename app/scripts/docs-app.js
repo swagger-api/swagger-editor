0d7e05bc4d306ca46309b0308c7db81af714723d
Mohsen Azimi <msn.azi@gmail.com>
2015-01-23 18:50:13 -0800

    [docs] Add docs.html and docsOnly mode to app
    This will allow downloading the docs as a embedded document

---
+++ app/scripts/docs-app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+window.SwaggerEditor = angular.module('SwaggerEditor', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ui.router',
+  'ui.bootstrap',
+  'ngStorage',
+  'ngSanitize',
+  'jsonFormatter',
+  'hc.marked',
+  'mohsen1.json-schema-view',
+  'schemaForm'
+]);
