2655b696f7683e8f347d38c791b894312466849d
Mohsen Azimi <msn.azi@gmail.com>
2015-02-05 12:54:20 -0800

    [analytics] track open example

--- app/scripts/controllers/openexamples.js
+++ app/scripts/controllers/openexamples.js
@@ -2,12 +2,15 @@
 
 SwaggerEditor.controller('OpenExamplesCtrl', function OpenExamplesCtrl($scope,
   $modalInstance, $rootScope, FileLoader, Builder, Storage, ASTManager,
-  defaults) {
+  Analytics, defaults) {
 
   $scope.files = defaults.exampleFiles;
   $scope.selectedFile = defaults.exampleFiles[0];
 
   $scope.open = function (file) {
+
+    Analytics.sendEvent('open-example', 'open-example:' + file);
+
     FileLoader.loadFromUrl('spec-files/' + file).then(function (value) {
       Storage.save('yaml', value);
       ASTManager.refresh(value);
