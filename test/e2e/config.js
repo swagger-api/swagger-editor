78803d89b0f231c3a682552b7d1010adcb17e027
Mohsen Azimi <msn.azi@gmail.com>
2015-02-16 14:03:04 -0800

    [test] update protractor's custome reporter loading mechanism
    Protractor is now using Jasmine 2.x

--- test/e2e/config.js
+++ test/e2e/config.js
@@ -14,8 +14,8 @@ var config = {
     // The require statement must be down here, since jasmine-reporters
     // needs jasmine to be in the global and protractor does not guarantee
     // this until inside the onPrepare function.
-    require('jasmine-reporters');
-    jasmine.getEnv().addReporter(new jasmine.TapReporter());
+    var jasmineReporter = require('jasmine-reporters');
+    jasmine.getEnv().addReporter(new jasmineReporter.TapReporter());
   },
 
   specs: ['specs/**/*test.js'],
