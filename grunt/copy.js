714b2c3e31a0bd78f9586a27741be95b08b9744f
Mohsen Azimi <msn.azi@gmail.com>
2015-02-16 12:34:16 -0800

    [build] copy views and templates to dist
    Artifact of removing docs-only mode

--- grunt/copy.js
+++ grunt/copy.js
@@ -13,6 +13,8 @@ module.exports = {
         '*.html',
         'config/defaults.json',
         'images/{,*/}*.{webp}',
+        'views/{,*/}*.html',
+        'templates/{,*/}*.html',
         'fonts/*',
         'schema/*',
         'spec-files/*',
