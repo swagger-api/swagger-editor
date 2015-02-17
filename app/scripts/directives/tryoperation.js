c495d648b65d1ee310ed1f30d8302028962bccbb
Armin Fasold <a.fasold@packetwerk.de>
2015-02-17 12:04:24 +0100

    Fix for #302: getSecurityOptions() now falls back to root security object.

--- app/scripts/directives/tryoperation.js
+++ app/scripts/directives/tryoperation.js
@@ -362,6 +362,10 @@ SwaggerEditor.controller('TryOperation', function ($scope, formdataFilter,
       return none.concat($scope.operation.security.map(function (security) {
         return Object.keys(security)[0];
       }));
+    } else if (Array.isArray($scope.specs.security)) {
+      return none.concat($scope.specs.security.map(function (security) {
+        return Object.keys(security)[0];
+      }));
     }
     return none;
   };
