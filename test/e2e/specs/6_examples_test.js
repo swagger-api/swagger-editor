3d84e9456fa049573fa06e9c178d42b129245572
Mohsen Azimi <msn.azi@gmail.com>
2015-03-11 15:49:00 -0700

    [test] add failing end-to-end tests for examples

---
+++ test/e2e/specs/6_examples_test.js
@@ -0,0 +1,34 @@
+'use strict';
+
+
+describe('Example', function () {
+  [
+    'Uber API',
+    'PetStore on Heroku',
+    'Simple API',
+    'Echo',
+    'Swagger Petstore (Simple)',
+    'Swagger Petstore',
+    'Basic Auth Example',
+    'Swagger Sample API'
+  ].forEach(testExample);
+});
+
+function testExample(title, index) {
+  describe(title, function(){
+    it('should open example', function(){
+      $('#fileMenu').click();
+      $('#open-example').click();
+      $('.modal-dialog select').click();
+      $('.modal-dialog select option:nth-child(' + (index+1) + ')').click();
+      $('.modal-dialog .btn.btn-primary').click();
+
+      expect($('.modal-dialog').isPresent()).toBe(false);
+      expect($('.info-header').getText()).toContain(title);
+    });
+
+    it('should show no errors', function(){
+      expect($('.error-presenter').isPresent()).toBe(false);
+    });
+  });
+}
