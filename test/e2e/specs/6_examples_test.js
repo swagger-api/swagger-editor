80c4167865a9e3b48b975e3001d4b137b4eebf1d
Mohsen Azimi <msn.azi@gmail.com>
2015-03-11 16:00:29 -0700

    JSCSify

--- test/e2e/specs/6_examples_test.js
+++ test/e2e/specs/6_examples_test.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 describe('Example', function () {
   [
     'Uber API',
@@ -15,19 +14,19 @@ describe('Example', function () {
 });
 
 function testExample(title, index) {
-  describe(title, function(){
-    it('should open example', function(){
+  describe(title, function () {
+    it('should open example', function () {
       $('#fileMenu').click();
       $('#open-example').click();
       $('.modal-dialog select').click();
-      $('.modal-dialog select option:nth-child(' + (index+1) + ')').click();
+      $('.modal-dialog select option:nth-child(' + (index + 1) + ')').click();
       $('.modal-dialog .btn.btn-primary').click();
 
       expect($('.modal-dialog').isPresent()).toBe(false);
       expect($('.info-header').getText()).toContain(title);
     });
 
-    it('should show no errors', function(){
+    it('should show no errors', function () {
       expect($('.error-presenter').isPresent()).toBe(false);
     });
   });
