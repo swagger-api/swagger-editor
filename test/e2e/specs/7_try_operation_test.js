c3f4dd371fc1880e8c025fbe89fb45ae1e4d2a6b
Mohsen Azimi <msn.azi@gmail.com>
2015-05-07 15:10:10 -0700

    [tests][try] Add E2E tests for try operation
    Fixes #285

---
+++ test/e2e/specs/7_try_operation_test.js
@@ -0,0 +1,73 @@
+'use strict';
+
+describe('Try Operation', function () {
+  it('opens "Heroku Pets" example', function () {
+    $('#fileMenu').click();
+    $('#open-example').click();
+    $('.modal-dialog select').click();
+
+    // heroku pets is second one
+    $('.modal-dialog select option:nth-child(2)').click();
+    $('.modal-dialog .btn.btn-primary').click();
+
+    expect($('.modal-dialog').isPresent()).toBe(false);
+    expect($('.info-header').getText()).toContain('PetStore on Heroku');
+  });
+
+  it('should show no errors', function () {
+    expect($('.error-presenter').isPresent()).toBe(false);
+  });
+
+  it('opens try this operation for GET /', function () {
+    $('ul.paths > li:nth-child(1) li.get.operation .try-operation > button')
+      .click();
+    browser.sleep(4000);
+    expect($('.try-container').isPresent()).toBe(true);
+  });
+
+  it('renders the form for "limit" parameter', function () {
+    expect(
+      $('.try-container input[name="root[parameters][limit]"]')
+        .isPresent()
+    ).toBe(true);
+  });
+
+  it('renders correct request URL', function () {
+    expect($('.try-container .raw-request .line.url a').getText())
+      .toContain('http://petstore-api.herokuapp.com/pet/?limit=11');
+  });
+
+  it('changing the scheme changes the request URL', function () {
+    $('.try-container select[name="root[scheme]"]').click();
+    $('.try-container select[name="root[scheme]"] option:nth-child(2)').click();
+
+    expect($('.try-container .raw-request .line.url a').getText())
+      .toContain('https://petstore-api.herokuapp.com');
+  });
+
+  it('updating the limit value updates request URL', function () {
+    var limitInput = $('.try-container input[name="root[parameters][limit]"]');
+
+    limitInput.clear();
+    limitInput.sendKeys('20');
+
+    // to release focus from the input and trigger the value change
+    $('.main-header').click();
+
+    expect($('.try-container .raw-request .line.url a').getText())
+      .toContain('limit=20');
+  });
+
+  it('sends the request and response appears', function () {
+    $('button[ng-click="makeCall()"]').click();
+
+    browser.sleep(3000); // wait for the XHR call
+
+    // renders the headers
+    expect($('[json="responseHeaders"]').isPresent()).toBe(true);
+
+    // renders the body
+    expect($('[json="responseData"]').isPresent()).toBe(true);
+
+  });
+});
