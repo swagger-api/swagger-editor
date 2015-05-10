d14d4a798c3d81383a73cea39262785488d583e9
Mohsen Azimi <msn.azi@gmail.com>
2015-05-10 15:34:36 -0700

    [autocomplete] clean up autocompelte service

--- app/scripts/services/autocomplete.js
+++ app/scripts/services/autocomplete.js
@@ -1,9 +1,64 @@
 'use strict';
 
+/*
+ * Autocomplete service extends Ace's completion mechanism to provide more
+ * relevant completion candidates based on Swagger document.
+*/
 SwaggerEditor.service('Autocomplete', function ($rootScope, snippets,
   ASTManager, KeywordMap) {
   var editor = null;
 
+  // Ace KeywordCompleter object
+  var KeywordCompleter = {
+
+    // this method is being called by Ace to get a list of completion candidates
+    getCompletions: function (editor, session, pos, prefix, callback) {
+
+      // Let Ace select the first candidate
+      editor.completer.autoSelect = true;
+
+      var keywordsForPos = getKeywordsForPosition(pos);
+      var snippetsForPos = getSnippetsForPosition(pos);
+
+      callback(null, keywordsForPos.concat(snippetsForPos));
+    }
+  };
+
+  /*
+   * Initializes autocomplete service. This method should be called after editor
+   *   is ready
+   *
+   * @param {object} e - the Ace editor object
+  */
+  this.init = function (e) {
+    editor = e;
+    editor.completers = [KeywordCompleter];
+  };
+
+  /*
+   * Construct an Ace compatible snippet from a snippet object that is made from
+   *  snippets made from our snippets
+   *
+   * @param {object} snippet - a snippet from snippet.js file
+   *
+   * @returns {object} - an Ace compatible snippet object
+  */
+  function constructAceSnippet(snippet) {
+    return {
+      caption: snippet.name,
+      snippet: snippet.content,
+      meta: 'snippet'
+    };
+  }
+
+  /*
+   * Gets keyword path for specified position
+   *
+   * @param {position} pos - AST Mark position
+   *
+   * @returns {array} - a list of keywords to reach to provided position based
+   *   in the YAML document
+  */
   function getPathForPosition(pos) {
     // pos.column is 1 more because the character is already inserted
     var path = ASTManager.pathForPosition(pos.row, pos.column - 1);
@@ -33,7 +88,12 @@ SwaggerEditor.service('Autocomplete', function ($rootScope, snippets,
   }
 
   /*
-  * Get filter function for snippets based on a position (`pos`)
+   * Gest filter function for snippets based on a cursor position
+   *
+   * @param {object} - cursor position
+   *
+   * @returns {function} - filter function for selection proper snippets based
+   *  on provided position
   */
   function filterForSnippets(pos) {
     ASTManager.refresh($rootScope.editorValue);
@@ -52,8 +112,12 @@ SwaggerEditor.service('Autocomplete', function ($rootScope, snippets,
   }
 
   /*
-   * With a given object returns child that it's key
-   * matches `key`
+   * With a given object returns the child that it's key matches provided key
+   *
+   * @param {object} object - the object to look into
+   * @param {key} - the key used for lookup
+   *
+   * @returns {object} - the object that is matched
   */
   function getChild(object, key) {
     var keys = Object.keys(object);
@@ -69,22 +133,30 @@ SwaggerEditor.service('Autocomplete', function ($rootScope, snippets,
   }
 
   /*
-   * Get array of keywords base don a position (`pos`)
+   * Gets array of keywords based on a position
+   *
+   * @param {object} pos - the position to get keywords from
+   *
+   * @returns {array} - list of keywords for provided position
   */
   function getKeywordsForPosition(pos) {
     var path = getPathForPosition(pos);
     var keywordsMap = KeywordMap.get();
     var key = path.shift();
 
+    // is getting path was not successful stop here and return no candidates
     if (!Array.isArray(path)) {
       return [];
     }
 
+    // traverse down the keywordsMap for each key in the path until there is no
+    // key in the path
     while (key && angular.isObject(keywordsMap)) {
       keywordsMap = getChild(keywordsMap, key);
       key = path.shift();
     }
 
+    // if no keywordsMap was found after the traversal return no candidates
     if (!angular.isObject(keywordsMap)) {
       return [];
     }
@@ -95,49 +167,81 @@ SwaggerEditor.service('Autocomplete', function ($rootScope, snippets,
       keywordsMap = keywordsMap[0];
     }
 
-    var result = Object.keys(keywordsMap).map(function (keyword) {
-      return {
-        name: keyword,
-        value: keyword,
-        score: 300,
-        meta: 'swagger'
-      };
-    });
+    // if keywordsMap is not an object at this point return no candidates
+    if (!angular.isObject(keywordsMap)) {
+      return [];
+    }
 
-    return result;
+    // for each key in keywordsMap map construct a completion candidate and
+    // return the array
+    return Object.keys(keywordsMap).map(constructAceCompletion);
   }
 
   /*
-   * Gives score to snippet based on their position
-   * FIXME: right now it gives 100 to any snippet.
+   * Constructs an Ace compatible completion candidate from a keyword
+   *
+   * @param {string} keyword
+   *
+   * @returns {object} - Ace compatible completion candidate
   */
-  function sortSnippets(snippet) {
-    snippet.score = 1000;
-    return snippet;
+  function constructAceCompletion(keyword) {
+    return {
+      name: keyword,
+      value: keyword,
+      score: 300,
+      meta: 'swagger'
+    };
   }
 
-  var KeywordCompleter = {
-    getCompletions: function (editor, session, pos, prefix, callback) {
-      editor.completer.autoSelect = true;
+  /*
+   * Gets array of snippets based on a position
+   *
+   * @param {object} pos - the position to get snippets from
+   *
+   * @returns {array} - list of snippets for provided position
+  */
+  function getSnippetsForPosition(pos) {
 
-      var snippetsForPos = snippets.filter(filterForSnippets(pos))
-        .map(function (snippet) {
-          return {
-            caption: snippet.name,
-            snippet: snippet.content,
-            meta: 'snippet'
-          };
-        })
-        .map(sortSnippets);
+    // find all possible snippets, modify them to be compatible with Ace and
+    // sort them based on their position. Sorting is done by assigning a score
+    // to each snippet, not by sorting the array
+    return snippets
+      .filter(filterForSnippets(pos))
+      .map(constructAceSnippet)
+      .map(snippetSorterForPos(pos));
+  }
 
-      var keywordsForPos = getKeywordsForPosition(pos);
+  /*
+   * Returns a function that gives score to snippet based on their position
+   *
+   * Note: not fully implemented method
+   *
+   * @param {object} position - current cursor position
+   *
+   * @returns {function} - applies snippet with score based on position
+  */
+  function snippetSorterForPos(position) {
 
-      callback(null, keywordsForPos.concat(snippetsForPos));
-    }
-  };
+    var path = getPathForPosition(position);
 
-  this.init = function (e) {
-    editor = e;
-    editor.completers = [KeywordCompleter];
-  };
+    // this function is used in Array#map
+    return function sortSnippets(snippet) {
+
+      // by default score is high
+      var score = 1000;
+
+      // if snippets content has the keyword it will get a lower score because
+      // it's more likely less relevant
+      // FIXME: is this logic work for all cases?
+      path.forEach(function (keyword) {
+        if (snippet.snippet.indexOf(keyword)) {
+          score = 500;
+        }
+      });
+
+      snippet.score = score;
+
+      return snippet;
+    };
+  }
 });
