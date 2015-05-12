94f82d43a2763e6520ef94c1d48f3cfd5bcb2c03
Mohsen Azimi <msn.azi@gmail.com>
2015-05-12 10:30:55 -0700

    [tests] add test for open example controller that checks for defaults

--- test/unit/spec/controllers/openexamples.js
+++ test/unit/spec/controllers/openexamples.js
@@ -6,6 +6,7 @@ describe('Controller: OpenExamplesCtrl', function () {
   beforeEach(window.angular.mock.module('SwaggerEditor'));
 
   var OpenExamplesCtrl,
+    FileLoader,
     $modalInstance,
     scope;
 
@@ -22,14 +23,39 @@ describe('Controller: OpenExamplesCtrl', function () {
         then: callback
       }
     };
+
+    var loadFromUrlCallback = sinon.spy();
+
+    FileLoader = {
+      loadFromUrl: function () {
+        return {
+          then: function () {}
+        }
+      }
+    };
+
     OpenExamplesCtrl = $controller('OpenExamplesCtrl', {
       $scope: scope,
-      $modalInstance: $modalInstance
+      $modalInstance: $modalInstance,
+      FileLoader: FileLoader
     });
   }));
 
   it('should have a scope', function () {
     expect(!!scope).to.equal(true);
-
   });
+
+  it('should select first example as selected file by default', function () {
+    expect(scope.selectedFile).to.equal('default.yaml');
+  });
+
+  it('honors defaults.examplesFolder configuration when opening files',
+    function () {
+      sinon.stub(FileLoader, 'loadFromUrl').returns({then: sinon.spy()});
+
+      scope.open('aFile');
+
+      expect(FileLoader.loadFromUrl).to.have.been.calledWithMatch('aFile');
+    }
+  );
 });
