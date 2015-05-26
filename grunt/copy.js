4ef5286735487567ee9721c14e7774073f861392
Mohsen Azimi <msn.azi@gmail.com>
2015-05-25 22:35:24 -0700

    [build] remove the schema folder
    Part of #462

--- grunt/copy.js
+++ grunt/copy.js
@@ -16,7 +16,6 @@ module.exports = {
         'views/{,*/}*.html',
         'templates/{,*/}*.html',
         'fonts/*',
-        'schema/*',
         'spec-files/*',
         'CNAME',
         'styles/branding.css',
