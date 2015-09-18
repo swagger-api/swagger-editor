121b8491c18b44be1ae6db871c2627da4a848558
Mohsen Azimi <me@azimi.me>
2015-09-18 15:18:31 -0700

    [performance] use the main thread for YAML#compose
    Due to this bug: https://github.com/connec/yaml-js/issues/17
    
    Related to:
    swagger-api/swagger-editor#641
    swagger-api/swagger-editor#658
    swagger-api/swagger-editor#651

--- app/scripts/services/yaml.js
+++ app/scripts/services/yaml.js
@@ -10,5 +10,15 @@ SwaggerEditor.service('YAML', function YAML() {
   // expose the methods that are being used
   this.load = worker.load.bind(worker);
   this.dump = worker.dump.bind(worker);
-  this.compose = worker.compose.bind(worker);
+
+  // Temporarily we are using the main thread to do the composition task due to
+  // this bug: https://github.com/connec/yaml-js/issues/17
+  var compose = _.memoize(window.yaml.compose);
+  this.compose = function (string, cb) {
+    try {
+      cb(null, compose(string));
+    } catch (error) {
+      cb(error);
+    }
+  };
 });
