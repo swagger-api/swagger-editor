e7b3f35f7ae599f7dff1ddefc074e2d6469feb9d
Sahar Jafari <shr.jafari@gmail.com>
2016-05-20 17:18:05 -0700

    Work in progress4, adding more test to TryOperation

--- test/unit/spec/controllers/tryoperation.js
+++ test/unit/spec/controllers/tryoperation.js
@@ -89,4 +89,38 @@ describe('Controller: TryOperation', function() {
       expect(scope.isJson(str)).to.equal(false);
     });
   });
+
+  describe('scope.isType', function() {
+    it('is a function', function() {
+      expect(scope.isType).to.be.a.function;
+    });
+
+    it('returns false for html type and image Content-Type', function() {
+      var header = {"Content-Type": "image/jpeg"};
+      var type = "html";
+
+      expect(scope.isType(header, type)).to.equal(false);
+    });
+
+    it('returns true for html type and text Content-Type', function() {
+      expect(scope.isType({"Content-Type": "text/html"}, "html"))
+      .to.equal(true);
+    });
+
+    it('returns true for json type and json Content-Type', function() {
+      expect(scope.isType({"Content-Type": "application/json"}, "json"))
+      .to.equal(true);
+    });
+
+    it('returns false for text type and json Content-Type', function() {
+      expect(scope.isType({"Content-Type": "application/json"}, "text"))
+      .to.equal(false);
+    });
+  });
+
+  describe('scope.isCrossOrigin', function() {
+    it('is a function', function() {
+      expect(scope.isCrossOrigin).to.be.a.function;
+    });
+  });
 });
