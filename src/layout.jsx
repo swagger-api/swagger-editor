3dbb2ba181c0b8f1db5c18dd3e0f29edba9a5523
Kyle Shockey <shockey@users.noreply.github.com>
2017-04-14 19:07:25 -0700

    Use UI's BaseLayout

--- src/layout.jsx
+++ src/layout.jsx
@@ -13,23 +13,11 @@ export default class EditorLayout extends React.Component {
   }
 
   render() {
-    let { specSelectors, getComponent } = this.props
+    let { getComponent } = this.props
 
-    let info = specSelectors.info()
-    let url = specSelectors.url()
-    let basePath = specSelectors.basePath()
-    let host = specSelectors.host()
-    let securityDefinitions = specSelectors.securityDefinitions()
-    let externalDocs = specSelectors.externalDocs()
+    let UIBaseLayout = getComponent("BaseLayout", true)
 
-    let Info = getComponent("info")
-    let Operations = getComponent("operations", true)
-    let Models = getComponent("models", true)
-    let AuthorizeBtn = getComponent("authorizeBtn", true)
     let Container = getComponent("Container")
-    let Row = getComponent("Row")
-    let Col = getComponent("Col")
-    let Errors = getComponent("errors", true)
     let Topbar = getComponent("Topbar", true)
     let EditorContainer = getComponent("EditorContainer", true)
     const SplitPaneMode = getComponent("SplitPaneMode", true)
@@ -40,32 +28,7 @@ export default class EditorLayout extends React.Component {
         <Container className='container'>
           <SplitPaneMode>
             <EditorContainer/>
-            <div className="swagger-ui">
-              <Errors/>
-              <Row>
-                <Col mobile={12}>
-                  { info.count() ? (
-                    <Info info={ info } url={ url } host={ host } basePath={ basePath } externalDocs={externalDocs} getComponent={getComponent}/>
-                  ) : null }
-                </Col>
-              </Row>
-              { securityDefinitions && <Row>
-                <Col mobile={12}>
-                  <AuthorizeBtn />
-                </Col>
-              </Row>
-            }
-            <Row>
-              <Col mobile={12} desktop={12} >
-                <Operations/>
-              </Col>
-            </Row>
-            <Row>
-              <Col mobile={12} desktop={12} >
-                <Models/>
-              </Col>
-            </Row>
-          </div>
+            <UIBaseLayout/>
         </SplitPaneMode>
       </Container>
     </div>
