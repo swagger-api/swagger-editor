bb256728d7348ba6b82ca00285836d93bdb43e9f
Kyle Shockey <shockey@users.noreply.github.com>
2017-07-06 11:33:20 -0700

    Disable Swagger 2 autocomplete when OAS3 is present

--- src/plugins/editor-autosuggest-snippets/get-completions.js
+++ src/plugins/editor-autosuggest-snippets/get-completions.js
@@ -3,11 +3,19 @@ import getSnippetsForPath from "./get-snippets-for-path"
 
 export default function getCompletions(editor, session, pos, prefix, cb, ctx, system) {
 
-  const { fn: { getPathForPosition } } = system
+  const { fn: { getPathForPosition }, specSelectors } = system
+  const { isOAS3 } = specSelectors
+
+  if(isOAS3 && isOAS3()) {
+    // isOAS3 selector exists, and returns false
+    return cb(null, null)
+  }
+
   const { AST } = ctx
   const editorValue = editor.getValue()
   const path = getPathForPosition({ pos, prefix, editorValue, AST})
 
   const suggestions = getSnippetsForPath({ path, snippets})
-  cb(null, suggestions)
+
+  return cb(null, suggestions)
 }
