073b41d1c3eb8c1d955b34125dfc3b9210da7158
Josh Ponelat <jponelat@gmail.com>
2017-09-27 08:33:34 +0200

    add validateParameterBadKeys

--- src/plugins/validate/actions.js
+++ src/plugins/validate/actions.js
@@ -54,6 +54,30 @@ export const validateTypeArrayRequiresItems = () => (system) => {
 }
 
 
+export const validateParameterBadKeys = () => (system) => {
+  return system.validateSelectors
+    .allParameters()
+    .then(nodes => {
+      return nodes.reduce((acc, node) => {
+        if(node.keys.includes("example")) {
+          acc.push({
+            level: "error",
+            message: "'example' field is not allowed in parameter",
+            path: [...node.path, "example"]
+          })
+        }
+        if(node.node.required !== true && node.node.in === "path") {
+          acc.push({
+            level: "error",
+            message: "Path parameters must have 'required: true'. You can always create another path/operation without this parameter to get the same behaviour.",
+            path: node.path
+          })
+        }
+        return acc
+      }, [])
+    })
+}
+
 export const validateMinAndMax = () => (system) => {
   return system.validateSelectors
     .allSchemas()
