bfc306eed5f611c9644a630fe99ae0662f8845d3
Kyle Shockey <shockey@users.noreply.github.com>
2017-11-21 12:09:14 -0800

    Fix $ref collector to work correctly for JSON

--- test/plugins/validation/semantic/index.js
+++ test/plugins/validation/semantic/index.js
@@ -3,16 +3,28 @@ import { runSemanticValidators } from "src/plugins/validation/semantic-validator
 
 import {
   PetstoreExpandedRaw,
-  PetstoreExpandedDereferenced
+  PetstoreExpandedDereferenced,
+  PetstoreExpandedYAML
 } from "../data/petstore-expanded.swagger.js"
 
 describe("semantic validation integration tests", () => {
-  describe("Swagger2 Petstore Expanded", () => {
-    it.only("should not return any problems about the petstore definition", () => {
+  describe.only("Swagger2 Petstore Expanded", () => {
+    it("should not return any problems about the petstore definition as JSON", () => {
       const res = runSemanticValidators({
         jsSpec: PetstoreExpandedRaw,
         resolvedSpec: PetstoreExpandedDereferenced,
         specStr: JSON.stringify(PetstoreExpandedRaw),
+        getLineNumberForPath: () => null
+      })
+
+      expect(res).toEqual([])
+    })
+
+    it("should not return any problems about the petstore definition as YAML", () => {
+      const res = runSemanticValidators({
+        jsSpec: PetstoreExpandedRaw,
+        resolvedSpec: PetstoreExpandedDereferenced,
+        specStr: PetstoreExpandedYAML,
         getLineNumberForPath: () => null
       })
 
