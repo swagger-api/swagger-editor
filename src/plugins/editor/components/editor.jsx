95eacdee18049c941d1cdb0a222541a142a7efde
Carson Swope <cswope@google.com>
2017-11-29 14:03:14 -0700

    Add explicit "prop-types" dependency so swagger-editor is compatible with react v16

--- src/plugins/editor/components/editor.jsx
+++ src/plugins/editor/components/editor.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from "react"
+import React from "react"
+import PropTypes from "prop-types"
 import AceEditor from "react-ace"
 import editorPluginsHook from "../editor-plugins/hook"
 import { placeMarkerDecorations } from "../editor-helpers/marker-placer"
