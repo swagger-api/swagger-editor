ec8d2f50e07faa833d09e50901ae6db8b39281b4
Josh Ponelat <jponelat@gmail.com>
2017-12-05 13:58:55 +0200

    Add empty list of tests??

---
+++ test/plugins/validate/operations.js
@@ -0,0 +1,27 @@
+// import expect from "expect"
+// import { validate } from "plugins/validation/semantic-validators/validators/operations"
+
+// TODO: hmm, these tests are empty
+describe.skip("validation plugin - semantic - operations", () => {
+
+  describe("Operations cannot have both a 'body' parameter and a 'formData' parameter", () => {
+
+  })
+
+  describe("Operations must have only one body parameter", () => {
+
+  })
+
+  describe("Operations must have only one body parameter", () => {
+
+  })
+
+  describe("Operations must have unique (name + in combination) parameters", () => {
+
+  })
+
+  describe("Integrations", () => {
+
+  })
+
+})
