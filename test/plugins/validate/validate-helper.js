352edf66b13063c36216d6d64ba491b7269874a0
Josh Ponelat <jponelat@gmail.com>
2017-12-12 15:59:06 +0200

    Remove the need to include whole apis/preset in tests

--- test/plugins/validate/validate-helper.js
+++ test/plugins/validate/validate-helper.js
@@ -1,20 +1,33 @@
 import ValidatePlugin from "plugins/validate"
-// TODO: TEMP: in case I need to modify swagger-ui on the fly
-// delete require.cache[require.resolve('swagger-ui')]
+import ASTPlugin from "plugins/ast"
 
 export default function validateHelper(spec) {
+
+  // TODO: TEMP: in case I need to modify swagger-ui on the fly
+  delete require.cache[require.resolve('swagger-ui')]
+
   return new Promise((resolve) => {
     const SwaggerUi = require("swagger-ui")
+
+    const DummySwaggerJsPlugin = {
+      fn: {
+        resolve: a => Promise.resolve(a)
+      }
+    }
     const system = SwaggerUi({
       spec,
+      presets: [],
       plugins: [
         SwaggerUi.plugins.SpecIndex,
         SwaggerUi.plugins.ErrIndex,
-        ValidatePlugin
+        DummySwaggerJsPlugin,
+        ASTPlugin,
+        ValidatePlugin,
+
       ]
     })
-
     system.validateActions.all()
     setTimeout(resolve.bind(null, system), 50)
   })
+
 }
