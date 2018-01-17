a265632255ff57d0167d164d835093b45f313d71
Josh Ponelat <jponelat@gmail.com>
2018-01-17 23:26:20 +0200

    performance: add buffer for semantic error callback (#1628)
    * add debounced buffer for semantic error callback
    
    A lot of wasted effort doing the operations immediately.
    
    * Minimize debounce delay in tests
    
    * LESS test debounce delay
    
    * Use CI flag to disable delay as well
    
    * Raise test harness semantic validation async waiting period
    
    * MOAR delay....

--- src/plugins/validate-semantic/actions.js
+++ src/plugins/validate-semantic/actions.js
@@ -1,6 +1,33 @@
 import forEach from "lodash/forEach"
+import debounce from "lodash/debounce"
 
 export const SOURCE = "semantic"
+
+// the test system does not tolerate slowness!
+const { NODE_ENV, CI } = process.env
+const DEBOUNCE_MS = (NODE_ENV === "test" || CI === "true") ? 0 : 30
+
+// System for buffering/batching errors
+var errorCollector = []
+const debNewSpecErrBatch = debounce(() => {
+  const system = errorCollector.system // Just a reference to the "latest" system
+  try {
+    errorCollector.forEach(obj => {
+      obj.line = obj.line || system.fn.AST.getLineNumberForPath(system.specSelectors.specStr(), obj.path)
+      obj.source = SOURCE
+    })
+    system.errActions.newSpecErrBatch(errorCollector)
+    errorCollector = [] // Clear stack
+  } catch(e) {
+    console.error(e)
+  }
+}, DEBOUNCE_MS)
+
+const bufferedNewSpecErrBatch = (system, obj) => {
+  errorCollector.push(obj)
+  errorCollector.system = system
+  debNewSpecErrBatch()
+}
 
 export const all = () => (system) => {
 
@@ -13,20 +40,16 @@ export const all = () => (system) => {
     source: SOURCE
   })
 
-  const cb = (obj) => setTimeout(() => {
-    obj.line = obj.line || system.fn.AST.getLineNumberForPath(system.specSelectors.specStr(), obj.path)
-    obj.source = SOURCE
-    system.errActions.newSpecErr(obj)
-  }, 0)
+  const errCb = (obj) => bufferedNewSpecErrBatch(system, obj)
 
   forEach(system.validateActions, (fn, name) => {
     if(name.indexOf("validateAsync") === 0) {
-      fn(cb) // Function send messages on its own, it won't be cached ( due to the nature of async operations )
+      fn(errCb) // Function send messages on its own, it won't be cached ( due to the nature of async operations )
     } else if(name.indexOf("validate") === 0) {
       Promise.resolve(fn())
         .then(validationObjs => {
           if(validationObjs) {
-            validationObjs.forEach(cb)
+            validationObjs.forEach(errCb)
           }
         })
     }
