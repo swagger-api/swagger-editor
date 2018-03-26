c25d111c53e2df83250257c49ae88d01869f0880
kyle <shockey@users.noreply.github.com>
2018-03-26 15:54:02 -0700

    break tag deployer out into its own `build-tools` script (#1722)

---
+++ build-tools/deploy-docker-hub-tag.sh
@@ -0,0 +1,12 @@
+if [ $DOCKER_HUB_USERNAME ]; then
+  docker login --username=$DOCKER_HUB_USERNAME --password=$DOCKER_HUB_PASSWORD;
+
+  if [ ! -z "$TRAVIS_TAG" ]; then
+    DOCKER_IMAGE_TAG=$TRAVIS_TAG;
+    docker build -t $DOCKER_IMAGE_NAME .;
+    docker tag $DOCKER_IMAGE_NAME $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG;
+    docker push $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG;
+    docker tag $DOCKER_IMAGE_NAME $DOCKER_IMAGE_NAME:latest;
+    docker push $DOCKER_IMAGE_NAME:latest;
+  fi;
+fi;
