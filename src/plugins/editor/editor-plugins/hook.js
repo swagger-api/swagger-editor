fbc41c8668389a3ddf5227217fe50775b76f8e7b
kyle <shockey@users.noreply.github.com>
2018-07-04 11:40:02 -0500

    fix: restore casing of editor plugin names (#1819)
    Fixes a bug introduced in #1764 that was preventing editor plugins from running

--- src/plugins/editor/editor-plugins/hook.js
+++ src/plugins/editor/editor-plugins/hook.js
@@ -4,9 +4,9 @@ import JsonToYaml from "./json-to-yaml"
 import TabHandler from "./tab-handler"
 
 const plugins = [
-  {fn: GutterClick, name: "GutterClick"},
-  {fn: JsonToYaml, name: "JsonToYaml"},
-  {fn: TabHandler, name: "TabHandler"},
+  {fn: GutterClick, name: "gutterClick"},
+  {fn: JsonToYaml, name: "jsonToYaml"},
+  {fn: TabHandler, name: "tabHandler"},
 ]
 
 export default function (editor, props = {}, editorPluginsToRun = [], helpers = {}) {
