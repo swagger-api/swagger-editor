swagger-api/swagger-editor

commit 2cc957f755a72fcc005c084f689810ce468d2571

Feature/add unit test to editor (#1764)
* wip: mocking out ace and testing editor

* change to mocking + where updateYaml is called

* add initial origin tests

* add userTypes,userSees,userUndo,userRedo methods on fakeAce

* moar tests! some skipped in prep of PR

* fix lint issues

* clean up editor a little and fix test

* add origin to layout

* remove origin prop from src/layout

* put origin prop inside editor-container

* tweak debounce in editor.jsx

* remove require.context from hook.js

* clean up

* fix lint issues and add consts

src/plugins/editor/editor-plugins/hook.js
@@ -1,24 +1,15 @@
-const request = require.context("./", true, /\.js$/)
-const plugins = []
+// TODO: Turn these into actions, that we can override
+import GutterClick from "./gutter-click"
+import JsonToYaml from "./json-to-yaml"
+import TabHandler from "./tab-handler"
 
-request.keys().forEach( function( key ){
-  if( key === "./hook.js" ) {
-    return
-  }
-
-  if( key.slice(2).indexOf("/") > -1) {
-    // skip files in subdirs
-    return
-  }
-
-  plugins.push({
-    name: toTitleCase(key),
-    fn: request(key).default
-  })
-})
+const plugins = [
+  {fn: GutterClick, name: "GutterClick"},
+  {fn: JsonToYaml, name: "JsonToYaml"},
+  {fn: TabHandler, name: "TabHandler"},
+]
 
 export default function (editor, props = {}, editorPluginsToRun = [], helpers = {}) {
-  // TODO: refactor require context system to direct plugin references
   plugins
     .filter(plugin => ~editorPluginsToRun.indexOf(plugin.name))
     .forEach( plugin => {
@@ -28,13 +19,4 @@ export default function (editor, props = {}, editorPluginsToRun = [], helpers = 
         console.error(`${plugin.name || ""} plugin error:`, e)
       }
     })
-}
-
-export function toTitleCase(str) {
-  return str
-    .split("-")
-    .map(substr => substr[0].toUpperCase() + substr.slice(1))
-    .join("")
-    .replace(/\.js/g, "") // .js
-    .replace(/\.\//g, "") // ./
 }

.babelrc
@@ -16,6 +16,10 @@
         "src": "src/plugins"
       },
       {
+        "expose": "test",
+        "src": "test"
+      },
+      {
         "expose": "src",
         "src": "src"
       }

src/layout.jsx
@@ -2,20 +2,21 @@ import React from "react"
 import PropTypes from "prop-types"
 import Dropzone from "react-dropzone"
 
+Dropzone.displayName = "Dropzone" // For testing
+
 export default class EditorLayout extends React.Component {
 
   static propTypes = {
     errSelectors: PropTypes.object.isRequired,
     errActions: PropTypes.object.isRequired,
     specActions: PropTypes.object.isRequired,
-    specSelectors: PropTypes.object.isRequired,
     getComponent: PropTypes.func.isRequired,
     layoutSelectors: PropTypes.object.isRequired,
     layoutActions: PropTypes.object.isRequired
   }
 
-  onChange = (newYaml) => {
-    this.props.specActions.updateSpec(newYaml)
+  onChange = (newYaml, origin="editor") => {
+    this.props.specActions.updateSpec(newYaml, origin)
   }
 
   onDrop = (acceptedFiles, rejectedFiles) => {
@@ -28,7 +29,7 @@ export default class EditorLayout extends React.Component {
       const reader = new FileReader()
       reader.onloadend = () => {
         const spec = reader.result
-        this.onChange(spec)
+        this.onChange(spec, "fileDrop")
       }
 
       reader.readAsText(file, "utf-8")
@@ -36,17 +37,17 @@ export default class EditorLayout extends React.Component {
   }
 
   render() {
-    let { getComponent } = this.props
+    const { getComponent } = this.props
 
-    let UIBaseLayout = getComponent("BaseLayout", true)
-
-    let Container = getComponent("Container")
-    let EditorContainer = getComponent("EditorContainer", true)
+    const UIBaseLayout = getComponent("BaseLayout", true)
+    const EditorContainer = getComponent("EditorContainer", true)
     const SplitPaneMode = getComponent("SplitPaneMode", true)
+
+    const Container = getComponent("Container")
 
     return (
       <div>
-        <Container className='container'>
+        <Container className="container">
           <Dropzone
             className="dropzone"
             accept=".yaml,application/json"

src/plugins/editor/components/editor-container.jsx
@@ -35,6 +35,7 @@ export default class EditorContainer extends React.Component {
         <Editor
           {...propsForEditor}
           value={specSelectors.specStr()}
+          origin={specSelectors.specOrigin()}
           editorOptions={editorOptions}
           specObject={specSelectors.specJson().toJS()}
           errors={errSelectors.allErrors()}

src/plugins/editor/components/editor.jsx
@@ -6,6 +6,8 @@ import { placeMarkerDecorations } from "../editor-helpers/marker-placer"
 import Im, { fromJS } from "immutable"
 import ImPropTypes from "react-immutable-proptypes"
 
+import win from "src/window"
+
 import isUndefined from "lodash/isUndefined"
 import omit from "lodash/omit"
 import isEqual from "lodash/isEqual"
@@ -31,14 +33,17 @@ export default function makeEditor({ editorPluginsToRun }) {
       super(props, context)
 
       this.editor = null
-      this.yaml = !isUndefined(props.value) ? [props.value] : []
-      this.debouncedOnChange = debounce(this.onChange, props.debounce)
+
+      this.debouncedOnChange = props.debounce > 0
+        ? debounce(props.onChange, props.debounce)
+        : props.onChange
     }
 
     static propTypes = {
       specId: PropTypes.string,
       value: PropTypes.string,
       editorOptions: PropTypes.object,
+      origin: PropTypes.string,
       debounce: PropTypes.number,
 
       onChange: PropTypes.func,
@@ -48,6 +53,8 @@ export default function makeEditor({ editorPluginsToRun }) {
       goToLine: PropTypes.object,
       specObject: PropTypes.object.isRequired,
 
+      editorActions: PropTypes.object,
+
       AST: PropTypes.object.isRequired,
 
       errors: ImPropTypes.list,
@@ -56,23 +63,16 @@ export default function makeEditor({ editorPluginsToRun }) {
     static defaultProps = {
       value: "",
       specId: "--unknown--",
+      origin: "not-editor",
       onChange: NOOP,
       onMarkerLineUpdate: NOOP,
       markers: {},
       goToLine: {},
       errors: fromJS([]),
+      editorActions: {onLoad(){}},
       editorOptions: {},
       debounce: 800 // 0.5 imperial seconds™
 
-    }
-
-
-    // This should be debounced, not only to prevent too many re-renders, but to also capture the this.yaml value, at the same time we'll call the upstream onChange
-    onChange = (value) => {
-      // Send it upstream ( this.silent is taken from react-ace module). It avoids firing onChange, when we update setValue
-      this.yaml = this.yaml.slice(0,2) // Keep it small
-      this.yaml.unshift(value) // Add this yaml onto a stack (in reverse ), so we can see if upstream sends us back something we just sent it!
-      this.props.onChange(value)
     }
 
     checkForSilentOnChange = (value) => {
@@ -82,6 +82,7 @@ export default function makeEditor({ editorPluginsToRun }) {
     }
 
     onLoad = (editor) => {
+
       const { props } = this
       const { AST, specObject } = props
 
@@ -92,6 +93,7 @@ export default function makeEditor({ editorPluginsToRun }) {
 
       // fixes a warning, see https://github.com/ajaxorg/ace/issues/2499
       editor.$blockScrolling = Infinity
+
 
       session.setUseWrapMode(true)
       session.on("changeScrollLeft", xPos => { // eslint-disable-line no-unused-vars
@@ -105,8 +107,11 @@ export default function makeEditor({ editorPluginsToRun }) {
 
       editor.setHighlightActiveLine(false)
       editor.setHighlightActiveLine(true)
-      this.syncOptionsFromState(this.props.editorOptions)
-      props.editorActions.onLoad({...props, langTools, editor})
+      this.syncOptionsFromState(props.editorOptions)
+      if(props.editorActions && props.editorActions.onLoad)
+        props.editorActions.onLoad({...props, langTools, editor})
+
+      this.updateMarkerAnnotations(this.props)
     }
 
     onResize = () => {
@@ -130,7 +135,7 @@ export default function makeEditor({ editorPluginsToRun }) {
     }
 
     getWidth = () => {
-      let el = document.getElementById("editor-wrapper")
+      let el = win.document.getElementById("editor-wrapper")
       return el ? el.getBoundingClientRect().width : null
     }
 
@@ -161,21 +166,22 @@ export default function makeEditor({ editorPluginsToRun }) {
       })
     }
 
-    updateYaml = () => {
-      if(this.editor.getValue() === this.yaml[0]) {
-        // editor is already aware of latest changes, so do nothing
-        return
+    updateYamlIfOrigin = (props) => {
+      if(props.origin !== "editor") {
+        this.updateYaml(props)
       }
+    }
 
+    updateYaml = (props) => {
       // this.silent is taken from react-ace module. It avoids firing onChange, when we update setValue
       this.silent = true
       const pos = this.editor.session.selection.toJSON()
-      this.editor.setValue(this.yaml[0]) // The first element is the most recent
+      this.editor.setValue(props.value)
       this.editor.session.selection.fromJSON(pos)
       this.silent = false
     }
 
-    syncOptionsFromState = (editorOptions) => {
+    syncOptionsFromState = (editorOptions={}) => {
       const { editor } = this
       if(!editor) {
         return
@@ -183,6 +189,7 @@ export default function makeEditor({ editorPluginsToRun }) {
 
       const setOptions = omit(editorOptions, ["readOnly"])
       editor.setOptions(setOptions)
+
 
       const readOnly = isUndefined(editorOptions.readOnly)
             ? false
@@ -193,75 +200,54 @@ export default function makeEditor({ editorPluginsToRun }) {
     componentWillMount() {
       // add user agent info to document
       // allows our custom Editor styling for IE10 to take effect
-      var doc = document.documentElement
-      doc.setAttribute("data-useragent", navigator.userAgent)
+      var doc = win.document.documentElement
+      doc.setAttribute("data-useragent", win.navigator.userAgent)
       this.syncOptionsFromState(this.props.editorOptions)
     }
 
     componentDidMount() {
       // eslint-disable-next-line react/no-did-mount-set-state
-      this.width = this.getWidth()
-      document.addEventListener("click", this.onClick)
-      this.updateYaml()
 
-      if(this.props.markers) {
-        this.updateMarkerAnnotations(this.props)
-      }
+      this.width = this.getWidth()
+      win.document.addEventListener("click", this.onClick)
     }
 
     componentWillUnmount() {
-      document.removeEventListener("click", this.onClick)
+      win.document.removeEventListener("click", this.onClick)
     }
 
     componentWillReceiveProps(nextProps) {
       let hasChanged = (k) => !isEqual(nextProps[k], this.props[k])
       let wasEmptyBefore = (k) => nextProps[k] && (!this.props[k] || isEmpty(this.props[k]))
       const editor = this.editor
-      const newValue = nextProps.value
 
       // Change the debounce value/func
       if(this.props.debounce !== nextProps.debounce) {
-        this.debouncedOnChange.flush()
-        this.debouncedOnChange = debounce(this.onChange, nextProps.debounce)
+        if(this.debouncedOnChange.flush)
+          this.debouncedOnChange.flush()
+
+        this.debouncedOnChange = nextProps.debounce > 0
+          ? debounce(nextProps.onChange, nextProps.debounce)
+          : nextProps.onChange
       }
 
-      //// Mange the yaml lifecycle...
-      // If the yaml doesn't match _what we already have in state_ then update the yaml in the editor
-      // Taking care to manage the other things in lifecycle
-      if(newValue !== this.props.value && !this.yaml.indexOf(newValue) > -1) {
-
-        // Remove markers
-        if(this.removeMarkers) {
-          this.removeMarkers()
-        }
-
-        this.yaml = [newValue] // Clear our stack, and add the latest from props
-        this.updateYaml()
-
-        // Add back the markers
-        this.updateMarkerAnnotations(nextProps)
-
-        // Clear undo-stack if we've changed specId or it was empty before
-        if(hasChanged("specId") || wasEmptyBefore("value")) {
-          setTimeout(function () {
-            editor.getSession().getUndoManager().reset()
-          }, 100) // TODO: get rid of timeout
-        }
-      } else {
-        // Just update markers if they've changed
-        if(!Im.is(nextProps.markers, this.props.markers)) {
-          this.updateMarkerAnnotations(nextProps)
-        }
+      // Remove markers
+      if(this.removeMarkers) {
+        this.removeMarkers()
       }
 
-      // If the yaml was in our stack, we should clear it up
-      if(this.yaml.indexOf(newValue) > -1) {
-        // remove all previous yaml's ( leave newValue in though ).
-        // In case another onChange is still in flight
-        this.yaml = this.yaml.slice(this.yaml.indexOf(newValue) + 1)
-      }
+      this.updateYamlIfOrigin(nextProps)
 
+      // Add back the markers
+      this.updateMarkerAnnotations(nextProps)
       this.updateErrorAnnotations(nextProps)
+
+      // Clear undo-stack if we've changed specId or it was empty before
+      if(hasChanged("specId") || wasEmptyBefore("value")) {
+        setTimeout(function () {
+          editor.getSession().getUndoManager().reset()
+        }, 100) // TODO: get rid of timeout
+      }
 
       if(hasChanged("editorOptions")) {
         this.syncOptionsFromState(nextProps.editorOptions)
@@ -276,7 +262,6 @@ export default function makeEditor({ editorPluginsToRun }) {
 
     shouldComponentUpdate() {
       return false // Never update, see: componentWillRecieveProps and this.updateYaml for where we update things.
-      // TODO this might affect changes to the "onLoad", "onChange" props...
     }
 
     render() {
@@ -286,6 +271,7 @@ export default function makeEditor({ editorPluginsToRun }) {
         <AceEditor
           mode="yaml"
           theme="tomorrow_night_eighties"
+          value={this.props.value /* This will only load once, thereafter it'll be via updateYaml */}
           onLoad={this.onLoad}
           onChange={this.checkForSilentOnChange}
           name="ace-editor"

src/plugins/editor/index.js
@@ -3,13 +3,14 @@ import EditorContainer from "./components/editor-container"
 import * as actions from "./actions"
 import reducers from "./reducers"
 import * as selectors from "./selectors"
+import EditorSpecPlugin from "./spec"
 
 let Editor = makeEditor({
   editorPluginsToRun: ["gutterClick", "jsonToYaml", "pasteHandler"]
 })
 
 export default function () {
-  return {
+  return [EditorSpecPlugin, {
     components: { Editor, EditorContainer },
     statePlugins: {
       editor: {
@@ -18,5 +19,5 @@ export default function () {
         selectors
       }
     }
-  }
+  }]
 }

src/plugins/editor/spec.js
@@ -1,0 +1,34 @@
+const SPEC_UPDATE_ORIGIN = "spec_update_spec_origin"
+
+// wraps updateSpec to include the "origin" parameter, defaulting to "not-editor"
+// Includes a selector to get the origin, specSelectors.specOrigin
+export default function EditorSpecPlugin() {
+  return {
+    statePlugins: {
+      spec: {
+        wrapActions: {
+          updateSpec: (ori, system) => (specStr, origin) => {
+            system.specActions.updateSpecOrigin(origin)
+            ori(specStr)
+          }
+        },
+        reducers: {
+          [SPEC_UPDATE_ORIGIN]: (state, action) => {
+            return state.set("specOrigin", action.payload)
+          }
+        },
+        selectors: {
+          specOrigin: (state) => state.get("specOrigin") || "not-editor"
+        },
+        actions: {
+          updateSpecOrigin(origin="not-editor") {
+            return {
+              payload: origin+"",
+              type: SPEC_UPDATE_ORIGIN,
+            }
+          }
+        }
+      }
+    }
+  }
+}

src/window.js
@@ -1,0 +1,19 @@
+let win = {
+  navigator: {
+    userAgent: "fake!"
+  },
+  document: {
+    getElementById() {},
+    addEventListener() {},
+    removeEventListener() {},
+    documentElement: {
+      setAttribute() {},
+    }
+  },
+}
+
+if(typeof window !== "undefined") {
+  win = window
+}
+
+export default win

commit 6a37c913a54ac9c1025fd277c40e232c0c006c51

fix: restore casing of editor plugin names
Fixes a bug introduced in #1764 that was preventing editor plugins from running

src/plugins/editor/editor-plugins/hook.js
@@ -4,9 +4,9 @@ import JsonToYaml from "./json-to-yaml"
 import TabHandler from "./tab-handler"
 
 const plugins = [
-  {fn: GutterClick, name: "GutterClick"},
-  {fn: JsonToYaml, name: "JsonToYaml"},
-  {fn: TabHandler, name: "TabHandler"},
+  {fn: GutterClick, name: "gutterClick"},
+  {fn: JsonToYaml, name: "jsonToYaml"},
+  {fn: TabHandler, name: "tabHandler"},
 ]
 
 export default function (editor, props = {}, editorPluginsToRun = [], helpers = {}) {