7c0630c9f9dc183500e5039cd98a90c1708c34d0
Kyle Shockey <shockey@users.noreply.github.com>
2018-08-25 00:00:58 -0700

    fix: force HTTPS download session links for 2.0 io generator

---
+++ test/e2e/tests/bugs/1862.js
@@ -0,0 +1,101 @@
+describe("Editor #1862: codegen download links downgrade HTTPS", () => {
+  describe("in Swagger 2", () => {
+    beforeEach(() => {
+      cy.visit("/?url=/documents/petstore.swagger.yaml")
+
+      cy.server()
+
+      cy.route({
+        url: "*//generator.swagger.io/api/gen/servers",
+        response: ["nodejs"]
+      })
+
+      cy.route({
+        url: "*//generator.swagger.io/api/gen/clients",
+        response: ["javascript"]
+      })
+    })
+
+    it("should force HTTPS server downloads from Swagger.io Generator", () => {
+      let wasHttpHit = false
+      let wasHttpsHit = false
+
+      // Given
+
+      cy.route({
+        url: "https://generator.swagger.io/api/gen/servers/nodejs",
+        method: "POST",
+        response: {
+          "code": "a92bc815-f6e3-4a56-839b-fd2e6f379d52",
+          "link": "http://generator.swagger.io:80/api/gen/download/a92bc815-f6e3-4a56-839b-fd2e6f379d52"
+        }
+      })
+
+      cy.route({
+        url: "http://generator.swagger.io/api/gen/download/*",
+        onRequest: () => wasHttpHit = true,
+        response: {}
+      })
+
+      cy.route({
+        url: "https://generator.swagger.io/api/gen/download/*",
+        onRequest: () => wasHttpsHit = true,
+        response: {}
+      })
+
+      // Then
+      cy.contains("Generate Server")
+        .click()
+
+      cy.contains("nodejs")
+        .click()
+
+      cy.wait(100)
+        .then(() => {
+          expect(wasHttpHit).to.equal(false, "has HTTP server been hit")
+          expect(wasHttpsHit).to.equal(true, "has HTTPS server been hit")
+        })
+    })
+
+    it("should force HTTPS client downloads from Swagger.io Generator", () => {
+      let wasHttpHit = false
+      let wasHttpsHit = false
+
+      // Given
+
+      cy.route({
+        url: "https://generator.swagger.io/api/gen/clients/javascript",
+        method: "POST",
+        response: {
+          "code": "a92bc815-f6e3-4a56-839b-fd2e6f379d52",
+          "link": "http://generator.swagger.io:80/api/gen/download/a92bc815-f6e3-4a56-839b-fd2e6f379d52"
+        }
+      })
+
+      cy.route({
+        url: "http://generator.swagger.io/api/gen/download/*",
+        onRequest: () => wasHttpHit = true,
+        response: {}
+      })
+
+      cy.route({
+        url: "https://generator.swagger.io/api/gen/download/*",
+        onRequest: () => wasHttpsHit = true,
+        response: {}
+      })
+
+      // Then
+      cy.contains("Generate Client")
+        .click()
+
+      cy.contains("javascript")
+        .click()
+
+      cy.wait(100)
+        .then(() => {
+          expect(wasHttpHit).to.equal(false, "has HTTP server been hit")
+          expect(wasHttpsHit).to.equal(true, "has HTTPS server been hit")
+        })
+    })
+  })
+})
