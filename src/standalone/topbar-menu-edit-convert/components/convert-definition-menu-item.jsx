19990ccf4236423fe822505458fdc542bd6d2bf6
Kyle Shockey <shockey@users.noreply.github.com>
2019-03-08 20:22:01 -0800

    drop unused Portal reference

--- src/standalone/topbar-menu-edit-convert/components/convert-definition-menu-item.jsx
+++ src/standalone/topbar-menu-edit-convert/components/convert-definition-menu-item.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react"
 import PropTypes from "prop-types"
-import { Portal } from "react-portal"
 
 export default class ConvertDefinitionMenuItem extends Component {
   render() {
