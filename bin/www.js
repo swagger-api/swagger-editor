791ed8e6cc42f6973b563a694d5a8ef4a866e2d4
Peter Ryszkiewicz <pryszkie@gmail.com>
2019-06-13 13:36:00 -0500

    Fixed cwd of executable

--- bin/www.js
+++ bin/www.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
-require("child_process").exec("npm start")
+require("child_process").exec("npm start", {
+  cwd: __dirname
+}, (error, stdout, stderr) => {
+  console.error(error)
+  console.error(stderr)
+  // eslint-disable-next-line no-console
+  console.log(stdout)
+})
