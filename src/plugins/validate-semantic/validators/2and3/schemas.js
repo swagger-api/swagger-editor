9ccec0c7f36046d431290bcca83446cb4a17cd3f
Rafael Lopez <ralphdoe12@gmail.com>
2020-02-05 20:14:30 -0500

    bugfix/2098_allows_null_values_when_nullable pr updates

--- src/plugins/validate-semantic/validators/2and3/schemas.js
+++ src/plugins/validate-semantic/validators/2and3/schemas.js
@@ -32,34 +32,36 @@ export const validate2And3TypesInDefaultValuesMatchesWithEnum = () => (system) =
       return nodes.reduce((acc, node) => {
         const schemaObj = node.node
         const { type } = schemaObj || {}
-        const isNullable = schemaObj.nullable ? schemaObj.nullable : false
+        const isNullable = !!schemaObj.nullable
         const enumeration = schemaObj.enum
         if (enumeration !== null && typeof enumeration !== "undefined") {
           var enumIndex = 0
           enumeration.forEach((element, index) => {
             var isValidFormat = true
-            if ((element === null & !isNullable) || element !== null) {
-              if (type === "array" && (!Array.isArray(element) || element === null)) {
-                isValidFormat = false
-                enumIndex = index
-              } else if ((type === "number" || type === "string" || type === "boolean") && !(typeof element === type)) {
-                isValidFormat = false
-                enumIndex = index
-              } else if (type === "integer" && !Number.isInteger(element)) {
-                isValidFormat = false
-                enumIndex = index
-              } else if (type === "object" && ((element === null) || !(typeof element === type) || Array.isArray(element))) {
-                isValidFormat = false
-                enumIndex = index
-              }
-              if (!isValidFormat) {
-                acc.push({
-                  message: "enum value should conform to its schema's `type`",
-                  path: [...node.path, "enum", enumIndex],
-                  level: "warning",
-                })
-              }
+            if (element === null && isNullable) {
+              return
             }
+            if (type === "array" && (!Array.isArray(element) || element === null)) {
+              isValidFormat = false
+              enumIndex = index
+            } else if ((type === "number" || type === "string" || type === "boolean") && !(typeof element === type)) {
+              isValidFormat = false
+              enumIndex = index
+            } else if (type === "integer" && !Number.isInteger(element)) {
+              isValidFormat = false
+              enumIndex = index
+            } else if (type === "object" && ((element === null) || !(typeof element === type) || Array.isArray(element))) {
+              isValidFormat = false
+              enumIndex = index
+            }
+            if (!isValidFormat) {
+              acc.push({
+                message: "enum value should conform to its schema's `type`",
+                path: [...node.path, "enum", enumIndex],
+                level: "warning",
+              })
+            }
+
           }) 
         }
         return acc
