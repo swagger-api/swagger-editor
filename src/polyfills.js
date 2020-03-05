62e05feb23d72f38c732c8eff424f75f68a98385
barslev <benjaminbarslev@gmail.com>
2020-03-05 09:30:15 +0100

    Updated core-js to version 3

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,4 +1,4 @@
-require("core-js/fn/object/values")
-require("core-js/fn/object/assign")
-require("core-js/es6/string")
-require("core-js/es6/array")
+require("core-js/features/object/values")
+require("core-js/features/object/assign")
+require("core-js/es/string")
+require("core-js/es/array")
