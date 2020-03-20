079edac500890769c538e5cae09b78e8042c1e7f
Rafael Lopez <ralphdoe12@gmail.com>
2020-03-20 10:15:03 -0500

    2096 fixes, new validations and tests added

--- src/plugins/validate-semantic/validators/oas3/refs.js
+++ src/plugins/validate-semantic/validators/oas3/refs.js
@@ -6,7 +6,7 @@ export const validateOAS3RefsForRequestBodiesReferenceRequestBodyPositions = () 
         const value = node.node
         const ref = value.$ref
 
-        if(!ref) {
+        if (!ref) {
           return acc
         }
 
@@ -14,18 +14,50 @@ export const validateOAS3RefsForRequestBodiesReferenceRequestBodyPositions = () 
         const pathArr = refPath.split("/") || []
         const parentRefKey = pathArr.slice(-2)[0]
         const targetRefKey = pathArr.slice(-1)[0]
-        if(
-          targetRefKey !== "requestBody"
-          && parentRefKey !== "requestBody"
-          && parentRefKey !== "requestBodies"
-        ) {
+
+        // External references starting with http:// or https://
+        if (ref.startsWith("https://") || ref.startsWith("http://")){
+          return acc  
+        }
+
+        // External references starting with ./
+        if (ref.startsWith("./")){
+          return acc  
+        }
+
+        // Extensions are valid
+        if (ref.startsWith("#/") && (parentRefKey.startsWith("/x-") || targetRefKey.startsWith("/x-"))){
+          return acc  
+        }
+
+        // Edge cases
+        if (ref.endsWith("requestBody") && (ref.startsWith("#/paths") || ref.startsWith("#/components"))){
+          return acc  
+        }
+
+        // External references ending with yaml
+        if (ref.endsWith(".yaml")) {
+          return acc
+        }
+
+        // Starting with #/compontents/schemas is not allowed
+        if (ref.startsWith("#/components/schemas")) {
+          acc.push({
+            level: "error",
+            message: `requestBody $refs cannot point to '#/components/schemas/…', they must point to '#/components/requestBodies/…'`,
+            path: [...node.path, "$ref"]
+          })
+        } else
+        if (ref.startsWith("#/components") && !ref.startsWith("#/components/requestBodies/")) {
           acc.push({
             level: "error",
             message: `requestBody $refs must point to a position where a requestBody can be legally placed`,
             path: [...node.path, "$ref"]
           })
         }
+
         return acc
+        
       }, [])
     })
 }
