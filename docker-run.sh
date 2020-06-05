9ef38d9fb53a7c082d6da4198f78297152562b4c
kyle shockey <kyle.shockey1@gmail.com>
2020-06-04 22:56:28 -0400

    fix: bad conditional in `docker-run.sh` (#2169)
    * fix: bad conditional in `docker-run.sh`
    
    * MOAR quotes
    
    Co-authored-by: Kyle Shockey <shockey@users.noreply.github.com>

--- docker-run.sh
+++ docker-run.sh
@@ -8,8 +8,8 @@ INDEX_FILE=$NGINX_ROOT/index.html
 # TODO: this is empty but we'll be adding configuration values here
 
 ## Adding env var support for swagger file (json or yaml)
-if [[ -f $SWAGGER_FILE ]]; then
-  cp -s $SWAGGER_FILE $NGINX_ROOT
+if [[ -f "$SWAGGER_FILE" ]]; then
+  cp -s "$SWAGGER_FILE" "$NGINX_ROOT"
   REL_PATH="/$(basename $SWAGGER_FILE)"
   sed -i "s|https://petstore.swagger.io/v2/swagger.json|$REL_PATH|g" $INDEX_FILE
 fi
