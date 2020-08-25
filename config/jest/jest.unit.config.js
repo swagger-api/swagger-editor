ea4b0f47860d3b1590b7f7bc269ac84c7090f412
Timothy Lai <timothy.lai@gmail.com>
2020-08-25 12:49:13 -0700

    config: jest unit test

---
+++ config/jest/jest.unit.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  rootDir: path.join(__dirname, '..', '..'),
+  testEnvironment: 'jsdom',
+  testMatch: ['**/test/unit/*.js'],
+  // testMatch: ['**/test/unit/*.js', '**/test/unit/**/*.js'],
+  // setupFilesAfterEnv: ['<rootDir>/test/unit/setup.js'],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/test/build-artifacts/',
+    '<rootDir>/test/unit/setup.js',
+  ],
+};
