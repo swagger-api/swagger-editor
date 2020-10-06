00aaed9759c0b282516fe4ea58f1ced754a5744c
Luke <luke.hedgecox@aol.com>
2020-10-06 13:35:30 +0100

    Added React key prop

--- src/standalone/topbar-insert/forms/components/InsertForm.jsx
+++ src/standalone/topbar-insert/forms/components/InsertForm.jsx
@@ -6,9 +6,8 @@ import { checkForEmptyValue } from "../helpers/validation-helpers"
 const InsertForm = ({ formData, getComponent }) => {
   const InsertFormInput = getComponent("InsertFormInput")
   const formRows = []
-  let i = 0
   
-  formData.forEach((v) => {
+  formData.forEach((v, i) => {
     if (OrderedMap.isOrderedMap(v) || Map.isMap(v)) {
       // The form field has a prerequisite field that has been filled out.
       const dependsOnNonEmpty = v.has("dependsOn") && (!checkForEmptyValue(formData.getIn(v.get("dependsOn"))) || v.get("dependsOn") === "formData")
@@ -19,14 +18,13 @@ const InsertForm = ({ formData, getComponent }) => {
         const dependsOnValue = formData.getIn(v.get("dependsOn"))
         const updateOptions = v.get("updateOptions")
 
-        formRows.push(<InsertFormInput formData={v.set("options", updateOptions(dependsOnValue, formData))} index={i} getComponent={getComponent} />)
+        formRows.push(<InsertFormInput formData={v.set("options", updateOptions(dependsOnValue, formData))} index={i} getComponent={getComponent} key={i} />)
       } else if (!v.has("dependsOn") || (!(v.has("updateOptions") && v.has("options")) && dependsOnNonEmpty )) {
         // There is no prerequisite or the prerequisite has been filled out and there is no 
         // additional action to take, so simply show the form field.
-        formRows.push(<InsertFormInput formData={v} index={i} getComponent={getComponent}/>)
+        formRows.push(<InsertFormInput formData={v} index={i} getComponent={getComponent} key={i} />)
       } 
     }
-    i+=1
   })
 
   return <div> {formRows} </div>
