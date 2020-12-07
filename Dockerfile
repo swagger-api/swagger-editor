6a68a8c3483ddfc6dee30d3fa89d6bad78e23a22
Leif Segen <lsegen@herzog.com>
2020-12-07 07:00:23 -0600

    Replicate most of how swagger-ui's image is built and run

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,25 @@
 FROM nginx:1.19-alpine
 
+RUN apk --no-cache add nodejs
+
 LABEL maintainer="fehguy"
 
+ENV PORT 8080
 ENV BASE_URL ""
 
-RUN mkdir -p /run/nginx
-
-COPY nginx.conf /etc/nginx/
+COPY ./docker/nginx.conf ./docker/cors.conf /etc/nginx/
 
 COPY ./index.html /usr/share/nginx/html/
 COPY ./dist/* /usr/share/nginx/html/
-COPY ./docker-run.sh /usr/share/nginx/
+COPY ./docker/run.sh /usr/share/nginx/
+COPY ./docker/configurator /usr/share/nginx/configurator
 
-RUN chmod +x /usr/share/nginx/docker-run.sh
+RUN chmod +x /usr/share/nginx/run.sh && \
+    chmod -R a+rw /usr/share/nginx && \
+    chmod -R a+rw /etc/nginx && \
+    chmod -R a+rw /var && \
+    chmod -R a+rw /var/run
 
 EXPOSE 8080
 
-CMD ["sh", "/usr/share/nginx/docker-run.sh"]
+CMD ["sh", "/usr/share/nginx/run.sh"]
--- docker/run.sh
+++ docker/run.sh
@@ -6,25 +6,39 @@ NGINX_ROOT=/usr/share/nginx/html
 INDEX_FILE=$NGINX_ROOT/index.html
 NGINX_CONF=/etc/nginx/nginx.conf
 
+node /usr/share/nginx/configurator $INDEX_FILE
+
 # TODO: this is empty but we'll be adding configuration values here
 
 if [[ "${BASE_URL}" != "/" ]]; then
   sed -i "s|location / {|location $BASE_URL {|g" $NGINX_CONF
 fi
 
+replace_in_index myApiKeyXXXX123456789 $API_KEY
+
 ## Adding env var support for swagger file (json or yaml)
 if [[ -f "$SWAGGER_FILE" ]]; then
   cp -s "$SWAGGER_FILE" "$NGINX_ROOT"
   REL_PATH="/$(basename $SWAGGER_FILE)"
   sed -i "s|SwaggerEditorBundle({|SwaggerEditorBundle({\n      url: '$REL_PATH',|g" $INDEX_FILE
+
+  if [[ -z "$SWAGGER_ROOT" ]]; then
+    SWAGGER_ROOT="$(dirname $SWAGGER_JSON)"
+  fi
+
+  if [[ "$BASE_URL" != "/" ]]
+  then
+    BASE_URL=$(echo $BASE_URL | sed 's/\/$//')
+    sed -i \
+      "s|#SWAGGER_ROOT|rewrite ^$BASE_URL(/.*)$ \$1 break;\n        #SWAGGER_ROOT|" \
+      $NGINX_CONF
+  fi
+  sed -i "s|#SWAGGER_ROOT|root $SWAGGER_ROOT/;|g" $NGINX_CONF
 fi
 
-if [[ "$BASE_URL" != "/" ]]
-then
-  BASE_URL=$(echo $BASE_URL | sed 's/\/$//')
-  sed -i \
-    "s|#SWAGGER_ROOT|rewrite ^$BASE_URL(/.*)$ \$1 break;\n        #SWAGGER_ROOT|" \
-    $NGINX_CONF
+# replace the PORT that nginx listens on if PORT is supplied
+if [[ -n "${PORT}" ]]; then
+    sed -i "s|8080|${PORT}|g" $NGINX_CONF
 fi
 
 # Gzip after replacements
