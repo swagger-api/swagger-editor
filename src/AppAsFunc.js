8c3731d3b8bf2ae07dd31cf3fa4b0850ab05dbaa
Timothy Lai <timothy.lai@gmail.com>
2020-12-08 10:46:29 -0800

    fix(editor): genericEditorPlugin layout no console errors

--- src/AppAsFunc.js
+++ src/AppAsFunc.js
@@ -1,9 +1,22 @@
 import SwaggerEditor from './plugins/generic-editor/SwaggerEditor';
 import SwaggerEditorStandalonePreset from './plugins/standalone';
+// import EditorLayout from './plugins/generic-editor/layout';
+import GenericEditorPlugin from './plugins/generic-editor';
+// import EditorPlugin from './plugins/monaco';
+// import SplitPaneModePlugin from './plugins/split-pane-mode';
+
+const plugins = {
+  // EditorPlugin,
+  // SplitPaneModePlugin,
+  GenericEditorPlugin,
+};
 
 const editor = SwaggerEditor({
-  layout: 'StandaloneLayout',
+  layout: 'EditorLayout',
   presets: [SwaggerEditorStandalonePreset],
+  // presets: [EditorLayout],
+  plugins: Object.values(plugins),
+  url: 'https://petstore.swagger.io/v2/swagger.json',
 });
 window.editor = editor;
 
