f61b913d0438d478130d91e90195cfab2df3e2f7
Leif Segen <leif.nabil.segen@gmail.com>
2020-12-11 18:48:31 -0600

    Enable BASE_URL
    - Add BASE_URL environment variable
    - Replicate pertinent portions of swagger-ui's nginx.conf and run.sh
    - Dockerfile: Replace deprecated MAINTAINER with LABEL
    - Dockerfile: Replace deprecated ADD with COPY

--- docker-run.sh
+++ docker-run.sh
@@ -2,16 +2,33 @@
 
 set -e
 
+BASE_URL=${BASE_URL:-/}
 NGINX_ROOT=/usr/share/nginx/html
 INDEX_FILE=$NGINX_ROOT/index.html
+NGINX_CONF=/etc/nginx/nginx.conf
 
-# TODO: this is empty but we'll be adding configuration values here
+if [[ "${BASE_URL}" != "/" ]]; then
+  sed -i "s|location / {|location $BASE_URL {|g" $NGINX_CONF
+fi
 
 ## Adding env var support for swagger file (json or yaml)
 if [[ -f "$SWAGGER_FILE" ]]; then
   cp -s "$SWAGGER_FILE" "$NGINX_ROOT"
   REL_PATH="/$(basename $SWAGGER_FILE)"
   sed -i "s|SwaggerEditorBundle({|SwaggerEditorBundle({\n      url: '$REL_PATH',|g" $INDEX_FILE
+
+  if [[ -z "$SWAGGER_ROOT" ]]; then
+    SWAGGER_ROOT="$(dirname $SWAGGER_JSON)"
+  fi
+
+  if [[ "$BASE_URL" != "/" ]]
+  then
+    BASE_URL=$(echo $BASE_URL | sed 's/\/$//')
+    sed -i \
+      "s|#SWAGGER_ROOT|rewrite ^$BASE_URL(/.*)$ \$1 break;\n        #SWAGGER_ROOT|" \
+      $NGINX_CONF
+  fi
+  sed -i "s|#SWAGGER_ROOT|root $SWAGGER_ROOT/;|g" $NGINX_CONF
 fi
 
 # Gzip after replacements
