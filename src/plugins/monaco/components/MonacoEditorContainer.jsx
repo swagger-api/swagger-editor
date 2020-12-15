c02bf9e5cbbd9fccf0aeec33b2fbebdaedad1199
Timothy Lai <timothy.lai@gmail.com>
2020-12-14 16:23:06 -0800

    feat(editor): editor value onChange renders in swagger-ui

--- src/plugins/monaco/components/MonacoEditorContainer.jsx
+++ src/plugins/monaco/components/MonacoEditorContainer.jsx
@@ -13,8 +13,13 @@ function noop() {} // export to utils later
 export default class MonacoEditorContainer extends PureComponent {
   constructor(props) {
     super(props);
-    this.initialValue = 'Welcome to Swagger Editor';
-    this.currentValue = '';
+    this.state = {
+      initialValue: 'Welcome to Swagger Editor',
+      currentValue: null,
+      specInitialized: false,
+    };
+    this.onChangeEditorValue = this.onChangeEditorValue.bind(this);
+    this.editorDidMount = this.editorDidMount.bind(this);
   }
 
   componentDidMount() {
@@ -22,32 +27,51 @@ export default class MonacoEditorContainer extends PureComponent {
   }
 
   componentDidUpdate() {
-    this.getInitialValueToLoad();
+    // console.log('container componentDidUpdate');
+    const { specInitialized } = this.state;
+    if (!specInitialized) {
+      this.getInitialValueToLoad();
+    }
   }
 
   getInitialValueToLoad = async () => {
     // Intent: await async availability of spec from swagger-ui
     // MonacoEditor should load this value just once
     // Subsequent changes should use onChange
-    // Test issue: With dev hot reload, on code change,
-    // initialValue appears to get reset to default in MonacoEditor,
-    // but appears to re-initialize spec correctly
-    // TBD, if MonacoEditor reinstantiates itself but ignoring value/prop
-    // Idea, we could send this method down as a prop, or just use the onChange
-    // Basically avoid possible lifecyle issue
-    // Test issue: we may need to debounce
     const { specSelectors } = this.props;
+    const { specInitialized, currentValue } = this.state;
     const spec = await specSelectors.specStr();
     if (!spec) {
       console.log('spec not available');
       return;
     }
-    if (this.currentValue !== spec) {
-      this.currentValue = spec;
+    if (specInitialized) {
+      console.log('spec already initialized (once only)');
+      return;
+    }
+    if (spec && !currentValue && currentValue !== spec) {
+      // this.currentValue = spec;
+      // console.log('currentValue !== spec, spec:', spec);
+      // console.log('currentValue !== spec, currentValue:', currentValue);
+      this.setState({ currentValue: spec, specInitialized: true });
       console.log('set initial spec done; should only be done once.');
     } else {
       console.log('already set. should not appear before initial spec done!!!*');
     }
+  };
+
+  onChangeEditorValue = (val) => {
+    console.log('attempt to onChangeEditorValue.');
+    const { specActions } = this.props;
+    // update swagger-ui state.spec
+    specActions.updateSpec(val);
+    // update editor value
+    this.setState({ currentValue: val });
+  };
+
+  editorDidMount = (editor) => {
+    editor.focus();
+    // console.log('container editor mounted, should focus');
   };
 
   render() {
@@ -62,16 +86,19 @@ export default class MonacoEditorContainer extends PureComponent {
     } = this.props; // wip: Remove line 1: "eslint-disable no-unused-vars" as this.props gets built out
     // const MonacoEditor = getComponent('MonacoEditor');
     // const monacoEditorOptions = {};
+    const { initialValue, currentValue } = this.state;
 
     return (
       <div id="editor-wrapper" className="editor-wrapper">
         <MonacoEditor
           language="json"
-          value={this.currentValue}
-          defaultValue={this.initialValue}
+          value={currentValue}
+          defaultValue={initialValue}
           height="90vh"
           width="50"
           options={{ theme: 'vs-light' }}
+          onChange={this.onChangeEditorValue}
+          editorDidMount={this.editorDidMount}
         />
       </div>
     );
@@ -83,11 +110,11 @@ MonacoEditorContainer.defaultProps = {
 };
 
 MonacoEditorContainer.propTypes = {
-  // specActions: PropTypes.object, // wip: add .isRequired when implemented
+  specActions: PropTypes.oneOfType([PropTypes.object]).isRequired,
   // configsSelectors: PropTypes.object, // wip: add .isRequired when implemented
   onChange: PropTypes.func,
   // fn: PropTypes.object,
-  specSelectors: PropTypes.oneOfType([PropTypes.object]).isRequired, // wip: add .isRequired when implemented
+  specSelectors: PropTypes.oneOfType([PropTypes.object]).isRequired,
   // errSelectors: PropTypes.object, // wip: add .isRequired when implemented
   // editorSelectors: PropTypes.object, // wip: add .isRequired when implemented
   // getComponent: PropTypes.func, // wip: add .isRequired when implemented
