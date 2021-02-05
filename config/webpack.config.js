bece40bcc5f6bda8f1e475f54107d6abacfc5d07
Timothy Lai <timothy.lai@gmail.com>
2021-02-05 15:03:22 -0800

    refactor(editor): wip. worker-loader to replace monaco-webpack-plugin

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -168,35 +168,37 @@ module.exports = function (webpackEnv) {
       : isEnvDevelopment && "cheap-module-source-map",
     // These are the "entry points" to our application.
     // This means they will be the "root" imports that are included in JS bundle.
-    entry: isEnvDevelopment && !shouldUseReactRefresh
-        ? [
-            // Include an alternative client for WebpackDevServer. A client's job is to
-            // connect to WebpackDevServer by a socket and get notified about changes.
-            // When you save a file, the client will either apply hot updates (in case
-            // of CSS changes), or refresh the page (in case of JS changes). When you
-            // make a syntax error, this client will display a syntax error overlay.
-            // Note: instead of the default WebpackDevServer client, we use a custom one
-            // to bring better experience for Create React App users. You can replace
-            // the line below with these two lines if you prefer the stock client:
-            //
-            // require.resolve('webpack-dev-server/client') + '?/',
-            // require.resolve('webpack/hot/dev-server'),
-            //
-            // When using the experimental react-refresh integration,
-            // the webpack plugin takes care of injecting the dev client for us.
-            webpackDevClientEntry,
-            // Finally, this is your app's code:
-            paths.appIndexJs,
-            // We include the app code last so that if there is a runtime error during
-            // initialization, it doesn't blow up the WebpackDevServer client, and
-            // changing JS code would still trigger a refresh.
-          ]
-        : paths.appIndexJs,
+    entry: {
+      main: isEnvDevelopment && !shouldUseReactRefresh
+            ? [
+                // Include an alternative client for WebpackDevServer. A client's job is to
+                // connect to WebpackDevServer by a socket and get notified about changes.
+                // When you save a file, the client will either apply hot updates (in case
+                // of CSS changes), or refresh the page (in case of JS changes). When you
+                // make a syntax error, this client will display a syntax error overlay.
+                // Note: instead of the default WebpackDevServer client, we use a custom one
+                // to bring better experience for Create React App users. You can replace
+                // the line below with these two lines if you prefer the stock client:
+                //
+                // require.resolve('webpack-dev-server/client') + '?/',
+                // require.resolve('webpack/hot/dev-server'),
+                //
+                // When using the experimental react-refresh integration,
+                // the webpack plugin takes care of injecting the dev client for us.
+                webpackDevClientEntry,
+                // Finally, this is your app's code:
+                paths.appIndexJs,
+                // We include the app code last so that if there is a runtime error during
+                // initialization, it doesn't blow up the WebpackDevServer client, and
+                // changing JS code would still trigger a refresh.
+              ]
+            : paths.appIndexJs,
       // 'editor.worker': [path.resolve('node_modules/monaco-editor/esm/vs/editor/editor.worker.js')],
       // 'apidom.worker': 'path.resolve(__dirname, './src/workers/apidom.js' // monorepo path? and/or /src?
       // 'json.worker': [path.resolve('node_modules/monaco-editor/esm/vs/language/json/json.worker')],
+      // 'javascript.worker': [path.resolve('node_modules/monaco-editor/esm/vs/language/typescript/ts.worker')],
       // 'yaml.worker': need to separately load different npm module, which MonacoWebpackPlugin bundles
-    // }, // for use when entry is an object
+    },
     output: {
       // The build folder.
       path: isEnvProduction ? paths.appBuild : undefined,
@@ -398,6 +400,16 @@ module.exports = function (webpackEnv) {
                 name: "static/media/[name].[hash:8].[ext]",
               },
             },
+            {
+              test: /\.worker\.js$/,
+              use: {
+                loader: require.resolve("worker-loader"),
+                // exclude: /node_modules/,
+                options: {
+                  name: "[name].[hash].js"
+                }
+              }
+            },
             // Process application JS with Babel.
             // The preset includes JSX, Flow, TypeScript, and some ESnext features.
             {
@@ -584,18 +596,21 @@ module.exports = function (webpackEnv) {
             : undefined
         )
       ),
-      new MonacoWebpackPlugin({
-        languages: ['json', 'yaml'],
-        // customLanguages: [{
-        //   label: 'apidom',
-        //   entry: main,
-        //   worker: {
-        //     id: 'apidom.worker',
-        //     label: 'apidom-worker',
-        //     entry: 'apidom'
-        //   }
-        // }]
-      }),
+      // new webpack.ContextReplacementPlugin(
+      //   /monaco-editor(\\|\/)esm(\\|\/)vs(\\|\/)editor(\\|\/)common(\\|\/)services/
+      // ),
+      // new MonacoWebpackPlugin({
+      //   languages: ['json', 'yaml'],
+      //   // customLanguages: [{
+      //   //   label: 'apidom',
+      //   //   entry: main,
+      //   //   worker: {
+      //   //     id: 'apidom.worker',
+      //   //     label: 'apidom-worker',
+      //   //     entry: 'apidom'
+      //   //   }
+      //   // }]
+      // }),
       // Inlines the webpack runtime script. This script is too small to warrant
       // a network request.
       // https://github.com/facebook/create-react-app/issues/5358
