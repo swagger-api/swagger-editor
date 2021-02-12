4ce38a55d9c916341ef6177ac174857b4ec25910
Timothy Lai <timothy.lai@gmail.com>
2021-02-12 14:39:06 -0800

    feat(editor): monaco workers imported via node_modules

---
+++ src/workers/json.worker.js
@@ -0,0 +1,3 @@
+import * as JsonWorker from 'monaco-editor/esm/vs/language/json/json.worker';
+
+export default JsonWorker;
---
+++ src/workers/ts.worker.js
@@ -0,0 +1,4 @@
+// javascript & typescript share the same worker
+import * as TsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker';
+
+export default TsWorker;
