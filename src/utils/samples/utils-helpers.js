f0cf712503325c8df972cadec72b953f0588cfc2
Timothy Lai <timothy.lai@gmail.com>
2021-03-09 14:22:19 -0800

    feat(editor): language utils-helper
    * fromPosition
    * toRange
    * fromRange

---
+++ src/utils/samples/utils-helpers.js
@@ -0,0 +1,31 @@
+export function fromPosition(position) {
+  if (!position) {
+    return null;
+  }
+  return { character: position.column - 1, line: position.lineNumber - 1 };
+}
+
+export function toRange(range) {
+  if (!range) {
+    return null;
+  }
+  return new Range(
+    range.start.line + 1,
+    range.start.character + 1,
+    range.end.line + 1,
+    range.end.character + 1
+  );
+}
+
+export function fromRange(range) {
+  if (!range) {
+    return null;
+  }
+  return {
+    start: {
+      line: range.startLineNumber - 1,
+      character: range.startColumn - 1,
+    },
+    end: { line: range.endLineNumber - 1, character: range.endColumn - 1 },
+  };
+}
