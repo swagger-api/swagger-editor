ff28a159dbf253891cd2b84501910efb64c016b0
Tim Lai <timothy.lai@gmail.com>
2021-04-14 16:30:36 -0700

    feat: replace regex with `isURL` from validator lib (#2576)

--- src/standalone/topbar-insert/forms/helpers/validation-helpers.js
+++ src/standalone/topbar-insert/forms/helpers/validation-helpers.js
@@ -1,4 +1,5 @@
 import { OrderedMap, List, Map } from "immutable"
+import { isURL } from "validator"
 
 export const checkForEmptyValue = (value) => {
   return !value || value === null || value === "" || !/\S/.test(value) || value === [] ||
@@ -104,7 +105,7 @@ export const checkForErrors = (formData) => {
 }
 
 export const validateUrl = (url) => {
-  return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(url)
+  return isURL(url)
 }
 
 export const validateAlphaNum = (string) => {
