d601abca1497e1f89c7310b5c13712db501585c2
Timothy Lai <timothy.lai@gmail.com>
2021-04-23 15:10:59 -0700

    refactor(editor): add disabled options to languageExtensionPoint

--- src/utils/monaco-adapter/config.js
+++ src/utils/monaco-adapter/config.js
@@ -2,4 +2,6 @@ export const languageID = 'apidom';
 
 export const languageExtensionPoint = {
   id: languageID,
+  // aliases: ['JSON', 'json', 'YAML', 'yaml'],
+  // mimetypes: ['application/json', 'application/yaml'],
 };
