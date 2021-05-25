a8cb6e0d8d0ba302d6d19392e7f838252fcdac41
Timothy Lai <timothy.lai@gmail.com>
2021-05-25 13:24:44 -0700

    feat(editor): migrate StandaloneLayout to functional component

--- src/plugins/standalone/StandaloneLayout.jsx
+++ src/plugins/standalone/StandaloneLayout.jsx
@@ -1,22 +1,18 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-// import './topbar.scss';
-// import './react-dd-menu.scss';
 import './style.main.scss';
 
-export default class StandaloneLayout extends PureComponent {
-  render() {
-    const { getComponent } = this.props;
-    const EditorLayout = getComponent('EditorLayout', true);
-    const Topbar = getComponent('Topbar', true);
+export default function StandaloneLayout(props) {
+  const { getComponent } = props;
+  const EditorLayout = getComponent('EditorLayout', true);
+  const Topbar = getComponent('Topbar', true);
 
-    return (
-      <div>
-        <Topbar />
-        <EditorLayout />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Topbar />
+      <EditorLayout />
+    </div>
+  );
 }
 
 StandaloneLayout.propTypes = {
