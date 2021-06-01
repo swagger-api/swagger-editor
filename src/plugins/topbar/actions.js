75abc84aa2f1f78dce3124d468e1743040c8614d
Timothy Lai <timothy.lai@gmail.com>
2021-06-01 11:16:27 -0700

    feat(editor): clear editor when yaml or empty case

--- src/plugins/topbar/actions.js
+++ src/plugins/topbar/actions.js
@@ -545,17 +545,25 @@ export const saveAsYaml = ({ overrideWarning }) => async (system) => {
 };
 
 export const clearEditor = () => async (system) => {
-  const { specActions } = system;
+  const { specActions, specSelectors } = system;
   // todo: using an empty string will throw an apidom parser error
   // we will want to detect for various specs, e.g. openapi, asyncapi
   // so for now, we'll use a nearly blank OAS3.0.x spec
-  // we also should detect for current yaml vs json, and keep as format
-  // for the minimal spec
-  const minimalSpec = {
+  const minimalSpecOas = {
     openapi: '3.0.2',
   };
-  specActions.updateSpec(JSON.stringify(minimalSpec));
-  // we should also update monaco value
+  const editorContent = specSelectors.specStr();
+  const languageSubType = getDefinitionLanguage({ data: editorContent });
+  if (!editorContent || languageSubType !== 'json') {
+    // default to yaml
+    // JSON String -> JS object
+    const jsContent = YAML.safeLoad(JSON.stringify(minimalSpecOas));
+    // JS Object -> YAML string
+    const yamlContent = YAML.safeDump(jsContent);
+    specActions.updateSpec(yamlContent, { lineWidth: -1 });
+  } else {
+    specActions.updateSpec(JSON.stringify(minimalSpecOas));
+  }
   return { data: 'success' };
 };
 
