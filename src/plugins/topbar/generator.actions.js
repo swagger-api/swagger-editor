9d0e1266c8297548f214f25e8d48297633947ccb
Timothy Lai <timothy.lai@gmail.com>
2021-06-03 10:57:01 -0700

    refactor(editor): migrate generator download methods

--- src/plugins/topbar/generator.actions.js
+++ src/plugins/topbar/generator.actions.js
@@ -1,4 +1,11 @@
-import { getGeneratorsList } from '../../utils/utils-http';
+import URL from 'url'; // wrapper for node method
+
+import {
+  getGeneratorsList,
+  getGenerator2Definition,
+  postGenerator3WithSpec,
+} from '../../utils/utils-http';
+import { getFileDownload } from '../../utils/utils-file-download';
 import { defaultFixtures } from './fixtures.actions';
 
 // Redux
@@ -44,6 +51,21 @@ const getSpecVersion = (system) => {
   }
 
   return { isOAS3, isSwagger2 };
+};
+
+// currently re-used
+const getConfigsWithDefaultFallback = (system) => {
+  let { swagger2GeneratorUrl, oas3GeneratorUrl, swagger2ConverterUrl } = system.getConfigs();
+  if (!swagger2GeneratorUrl) {
+    swagger2GeneratorUrl = defaultFixtures.swagger2GeneratorUrl;
+  }
+  if (!oas3GeneratorUrl) {
+    oas3GeneratorUrl = defaultFixtures.oas3GeneratorUrl;
+  }
+  if (!swagger2ConverterUrl) {
+    swagger2ConverterUrl = defaultFixtures.swagger2ConverterUrl;
+  }
+  return { swagger2GeneratorUrl, oas3GeneratorUrl, swagger2ConverterUrl };
 };
 
 const getSpecVersionString = ({ isOAS3, isSwagger2 }) => {
@@ -112,4 +134,122 @@ export const shouldReInstantiateGeneratorClient = ({ specVersion }) => (system) 
   return false;
 };
 
-export default { instantiateGeneratorClient, shouldReInstantiateGeneratorClient };
+// Begin section: download generated file
+
+const formatParsedUrl = ({ link }) => {
+  const downloadUrl = URL.parse(link);
+  // HACK: workaround for Swagger.io Generator 2.0's lack of HTTPS downloads
+  if (downloadUrl.hostname === 'generator.swagger.io') {
+    downloadUrl.protocol = 'https:';
+    delete downloadUrl.port;
+    delete downloadUrl.host;
+  }
+  const formattedUrl = URL.format(downloadUrl);
+  return formattedUrl;
+};
+
+const fetchGeneratorLinkOrBlob = async ({ isSwagger2, isOAS3, name, type, spec }) => {
+  // generator 3 will return a Blob
+  // generator 2 will return a json object
+  if (!spec) {
+    return { error: 'unable to create generator url. missing spec' };
+  }
+  if (!type) {
+    return { error: 'unable to create generator url. missing type' };
+  }
+  if (!name) {
+    return { error: 'unable to create generator url. missing name' };
+  }
+
+  if (isOAS3) {
+    // Generator 3 only has one generate endpoint for all types of things...
+    // Return an object with Blob, regardless of type (server/client)
+    const generatorData = await postGenerator3WithSpec({
+      url: defaultFixtures.oas3GenerateSpecUrl,
+      data: {
+        spec,
+        type: type.toUpperCase(),
+        lang: name,
+      },
+    });
+    if (generatorData.error) {
+      return generatorData; // res.body.error
+    }
+    return generatorData; // generatorData = { data: Blob}
+  }
+  if (isSwagger2) {
+    if (type !== 'server' || type !== 'client') {
+      return { error: 'unable to create generator url. invalid type' };
+    }
+    let urlWithType;
+    if (type === 'server') {
+      urlWithType = `${defaultFixtures.oas2GenerateSpecServersUrl}/${name}`;
+    }
+    if (type === 'client') {
+      urlWithType = `${defaultFixtures.oas2GenerateSpecClientsUrl}/${name}`;
+    }
+    // todo: verify not needed to JSON.stringify(spec)
+    const generatorData = await postGenerator3WithSpec({
+      url: urlWithType,
+      data: {
+        spec,
+        options: {
+          responseType: 'json',
+        },
+      },
+    });
+    if (generatorData.error) {
+      return generatorData; // res.body.error
+    }
+    // console.log('generator...swagger 2 case.. generatorData:', generatorData);
+    // expecting generatorData = { code: '', link: ''}
+    const link = formatParsedUrl({ link: generatorData.link });
+    return { link };
+  }
+  // default empty case
+  return { error: 'unable to create generator url' };
+};
+
+export const downloadGeneratedFile = ({ type, name }) => async (system) => {
+  const { specSelectors } = system;
+  const { swagger2GeneratorUrl, oas3GeneratorUrl } = getConfigsWithDefaultFallback(system);
+  const { isOAS3, isSwagger2 } = getSpecVersion(system);
+
+  const argsForGeneratorUrl = {
+    isOAS3,
+    isSwagger2,
+    swagger2GeneratorUrl,
+    oas3GeneratorUrl,
+  };
+  const spec = specSelectors.specJson();
+
+  const generatorLink = await fetchGeneratorLinkOrBlob({
+    isOAS3: argsForGeneratorUrl.isOAS3,
+    isSwagger2: argsForGeneratorUrl.isSwagger2,
+    name,
+    type,
+    spec,
+  });
+  if (generatorLink.error) {
+    return { error: generatorLink.error };
+  }
+
+  const filename = `${name}-${type}-generated.zip`;
+  if (generatorLink.link) {
+    // swagger2: check axios fetch responseType: 'blob', then download
+    const fetchedDataWithBlob = await getGenerator2Definition({ url: generatorLink.link });
+    if (fetchedDataWithBlob.data && fetchedDataWithBlob.data instanceof Blob) {
+      getFileDownload({ blob: fetchedDataWithBlob.data, filename });
+    }
+  } else if (generatorLink.data && generatorLink.data instanceof Blob) {
+    // oas3: generator3 returns blob already, so just download
+    getFileDownload({ blob: generatorLink.data, filename });
+  }
+  return { data: 'ok' };
+};
+
+export default {
+  instantiateGeneratorClient,
+  shouldReInstantiateGeneratorClient,
+  downloadGeneratedFile,
+};
