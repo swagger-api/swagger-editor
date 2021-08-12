c9fbd4c95231790c6e336c948961f17019aedf04
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-08-12 09:56:12 +0200

    test(mocha): fix running tests on Node.js >= 16
    The Node.js thread of mocha test got stuck after running it.
    
    Refs #2710

--- test/mocha/setup.js
+++ test/mocha/setup.js
@@ -1,3 +1,8 @@
+const Enzyme = require("enzyme")
+const Adapter = require("@wojtekmaj/enzyme-adapter-react-17")
+
+Enzyme.configure({ adapter: new Adapter() })
+
 if (typeof process === "object") {
   require("jsdom-global")()
 }
