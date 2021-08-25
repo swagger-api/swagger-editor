1476b9d7ca292ae84c3ba71d96275b99c30b8459
Timothy Lai <timothy.lai@gmail.com>
2021-08-25 16:34:05 -0700

    test(editor): fix Topbar.test

--- src/plugins/topbar/components/Topbar.test.jsx
+++ src/plugins/topbar/components/Topbar.test.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-// eslint-disable-next-line no-unused-vars
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 
 import Topbar from './Topbar';
 import LinkHome from './LinkHome'; // from swagger-ui
@@ -13,38 +12,41 @@ import GeneratorMenuDropdown from './GeneratorMenuDropdown';
 import SaveAsJsonOrYaml from './SaveAsJsonOrYaml';
 import * as topbarActions from '../actions';
 
-// mock es6 re-exports
-jest.mock('../actions');
+jest.mock('../actions', () => ({
+  getDefinitionLanguageFormat: jest.fn(),
+  shouldUpdateDefinitionLanguageFormat: jest.fn(),
+  instantiateGeneratorClient: jest.fn(),
+  shouldReInstantiateGeneratorClient: jest.fn(),
+  allowConvertDefinitionToOas3: jest.fn(),
+}));
 
-test('renders Topbar with required components', async () => {
-  // These two spies are needed to render the GeneratorMenuDropdown component
-  const spyGeneratorList = jest
-    .spyOn(topbarActions, 'instantiateGeneratorClient')
-    .mockImplementation(() => ({
-      servers: ['blue', 'brown'],
-      clients: ['apple', 'avocado'],
-      specVersion: 'some string',
-    }));
+const setup = ({
+  servers,
+  clients,
+  specVersion,
+  languageFormat,
+  shouldUpdate,
+  allowConvertDefinitionToOas3,
+} = {}) => {
+  topbarActions.getDefinitionLanguageFormat.mockReturnValue({
+    languageFormat: languageFormat || 'yaml',
+  });
+  topbarActions.shouldUpdateDefinitionLanguageFormat.mockReturnValue({
+    languageFormat: languageFormat || 'yaml',
+    shouldUpdate: shouldUpdate || false,
+  });
+  topbarActions.instantiateGeneratorClient.mockReturnValue({
+    servers: servers || ['blue', 'brown'],
+    clients: clients || ['apple', 'avocado'],
+    specVersion: specVersion || 'some string',
+  });
+  topbarActions.shouldReInstantiateGeneratorClient.mockReturnValue(false);
+  topbarActions.allowConvertDefinitionToOas3.mockReturnValue(allowConvertDefinitionToOas3 || false);
 
-  const spyShouldReInstantiate = jest
-    .spyOn(topbarActions, 'shouldReInstantiateGeneratorClient')
-    .mockImplementation(() => {
-      return false;
-    });
+  return { topbarActions };
+};
 
-  // These two spies are needed to render the FileMenuDropdown component
-  const spyLanguageFormat = jest
-    .spyOn(topbarActions, 'getDefinitionLanguageFormat')
-    .mockImplementation(() => ({
-      languageFormat: 'yaml',
-    }));
-  const spyShouldUpdateLanguageFormat = jest
-    .spyOn(topbarActions, 'shouldUpdateDefinitionLanguageFormat')
-    .mockImplementation(() => ({
-      languageFormat: 'yaml',
-      shouldUpdate: false,
-    }));
-
+const renderGeneratorMenuDropdown = async (props) => {
   const components = {
     LinkHome,
     DropdownMenu,
@@ -61,30 +63,42 @@ test('renders Topbar with required components', async () => {
       getComponent={(c) => {
         return components[c];
       }}
-      topbarActions={topbarActions}
+      {...props} // eslint-disable-line react/jsx-props-no-spreading
     />
   );
 
-  // GeneratorMenuDropdown:1: async http call
-  await waitFor(() => expect(spyGeneratorList).toBeCalled());
-  // top-level dropdown menu. currently 4 menu items.
-  const linkElement1 = screen.getByText(/File/i);
-  await waitFor(() => linkElement1);
-  expect(linkElement1).toBeInTheDocument();
-  const linkElement2 = screen.getByText(/Edit/i);
-  await waitFor(() => linkElement2);
-  expect(linkElement2).toBeInTheDocument();
-  // element dependent on async mock resolve
-  const linkElement3 = screen.getByText(/Generate Server/i);
-  await waitFor(() => linkElement3);
-  expect(linkElement3).toBeInTheDocument();
-  // element dependent on async mock resolve
-  const linkElement4 = screen.getByText(/Generate Client/i);
-  await waitFor(() => linkElement4);
-  expect(linkElement4).toBeInTheDocument();
-  // GeneratorMenuDropdown:2: action method call
-  expect(spyShouldReInstantiate).toBeCalled();
-  // FileMenuDropdown: action methods call
-  expect(spyLanguageFormat).toBeCalled();
-  expect(spyShouldUpdateLanguageFormat).toBeCalled();
+  await waitFor(() => expect(topbarActions.instantiateGeneratorClient).toBeCalled());
+  await waitFor(() => expect(topbarActions.getDefinitionLanguageFormat).toBeCalled());
+  await waitFor(() => expect(topbarActions.shouldUpdateDefinitionLanguageFormat).toBeCalled());
+
+  const serverMenu = screen.queryByText(/Generate Server/i);
+  const clientMenu = screen.queryByText(/Generate Client/i);
+  const fileMenu = screen.queryByText(/File/i);
+  const editMenu = screen.queryByText(/Edit/i);
+
+  return {
+    serverMenu,
+    clientMenu,
+    fileMenu,
+    editMenu,
+  };
+};
+
+afterAll(() => {
+  jest.unmock('../actions');
+});
+
+test('should render with required components', async () => {
+  const { topbarActions: actions } = setup({
+    languageFormat: 'json',
+    shouldUpdateDefinitionLanguageFormat: false,
+  });
+  const { serverMenu, clientMenu, fileMenu, editMenu } = await renderGeneratorMenuDropdown({
+    topbarActions: actions,
+  });
+
+  expect(serverMenu).toBeInTheDocument();
+  expect(clientMenu).toBeInTheDocument();
+  expect(fileMenu).toBeInTheDocument();
+  expect(editMenu).toBeInTheDocument();
 });
