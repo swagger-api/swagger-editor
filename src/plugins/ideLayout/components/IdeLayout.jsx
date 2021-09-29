869181b020552e4343cf1f0a1515127d96924227
Timothy Lai <timothy.lai@gmail.com>
2021-09-29 16:03:26 -0700

    refactor(editor): rename scss index files to _all

--- src/plugins/ideLayout/components/IdeLayout.jsx
+++ src/plugins/ideLayout/components/IdeLayout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import '../style.main.scss';
+import './_all.scss';
 
 export default function IdeLayout(props) {
   const { getComponent } = props;
