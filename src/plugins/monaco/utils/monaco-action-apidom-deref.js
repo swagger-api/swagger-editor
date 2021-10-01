8c5834ad5dfe2f1649f8a161b47c9924f999d06e
Timothy Lai <timothy.lai@gmail.com>
2021-10-01 11:10:13 -0700

    feat(editor): monaco-action-apidom-deref util

---
+++ src/plugins/monaco/utils/monaco-action-apidom-deref.js
@@ -0,0 +1,42 @@
+import { getLanguageService, isJsonDoc, FORMAT } from '@swagger-api/apidom-ls';
+import { TextDocument } from 'vscode-languageserver-textdocument';
+import YAML from 'js-yaml';
+
+import metadata from '../workers/apidom/metadata';
+
+export async function dereference(editor) {
+  const apidomContext = {
+    metadata: metadata(),
+  };
+  const languageService = getLanguageService(apidomContext);
+
+  try {
+    const textDoc = TextDocument.create(
+      editor.getModel().uri.toString(),
+      editor.getModel().getModeId(),
+      editor.getModel().getVersionId(),
+      editor.getModel().getValue()
+    );
+    const context = {
+      format: isJsonDoc(textDoc) ? FORMAT.JSON : FORMAT.YAML,
+      baseURI: window.location.href,
+    };
+    const result = await languageService.doDeref(textDoc, context);
+
+    if (!result) {
+      return { error: 'an error has occured for: dereference' };
+    }
+    if (!isJsonDoc(textDoc)) {
+      const tempjsContent = YAML.load(result);
+      const tempyamlContent = YAML.dump(tempjsContent);
+      editor.setValue(tempyamlContent);
+    } else {
+      editor.setValue(result);
+    }
+    return { data: 'ok' };
+  } catch (e) {
+    return { error: e.message };
+  }
+}
+
+export default { dereference };
