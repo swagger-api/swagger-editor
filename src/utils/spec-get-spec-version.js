4754487ab6e97da11b55662b02e005ad9baef23b
Tim Lai <timothy.lai@gmail.com>
2021-10-21 13:21:27 -0700

    fix: handle json case with whitespace for getSpecVersion (#197)

--- src/utils/spec-get-spec-version.js
+++ src/utils/spec-get-spec-version.js
@@ -11,15 +11,20 @@
 // Note: can adapt to explicitly handle different specs and versions
 const testForAsyncApi2SpecVersion = (src) => {
   const specMatchJson = src.startsWith('asyncapi', 2); // acccount for `"{`
+  const specMatchJsonWithWhitespace = src.startsWith('asyncapi', 5); // account for `{  "`
   const specMatchYaml = src.startsWith('asyncapi');
-  if (!specMatchJson && !specMatchYaml) {
+  if (!specMatchJson && !specMatchYaml && !specMatchJsonWithWhitespace) {
     return false;
   }
   // starts with, but is not `n.x.y` exactly
   // may want to gate against `n.z.z`, e.g. (OAS) `3.0.x` and exclude `3.1.x`
   const pos = src.indexOf('2.');
-  if (pos > 9 && pos < 12) {
+  if ((specMatchJson || specMatchYaml) && pos > 9 && pos < 12) {
     // expect pos = 10, but perhaps some input string variance with spaces
+    return true;
+  }
+  if (specMatchJsonWithWhitespace && pos > 16 && pos < 19) {
+    // expect pos = 17, but perhaps some input string variance with spaces
     return true;
   }
   return false;
@@ -27,15 +32,20 @@ const testForAsyncApi2SpecVersion = (src) => {
 
 const testForOas3_1SpecVersion = (src) => {
   const specMatchJson = src.startsWith('openapi', 2); // acccount for `"{`
+  const specMatchJsonWithWhitespace = src.startsWith('openapi', 5); // account for `{  "`
   const specMatchYaml = src.startsWith('openapi');
-  if (!specMatchJson && !specMatchYaml) {
+  if (!specMatchJson && !specMatchYaml && !specMatchJsonWithWhitespace) {
     return false;
   }
   // starts with, but is not `n.x.y` exactly
   // may want to gate against `n.z.z`, e.g. (OAS) `3.0.x` and exclude `3.1.x`
   const pos = src.indexOf('3.1.');
-  if (pos > 8 && pos < 12) {
+  if ((specMatchJson || specMatchYaml) && pos > 8 && pos < 12) {
     // expect pos = 9, but perhaps some input string variance with spaces
+    return true;
+  }
+  if (specMatchJsonWithWhitespace && pos > 15 && pos < 18) {
+    // expect pos = 16, but perhaps some input string variance with spaces
     return true;
   }
   return false;
@@ -54,31 +64,7 @@ export const getSpecVersion = (system) => {
   const editorContent = specSelectors.specStr().substring(0, 20) || '';
   const isAsyncApi2 = testForAsyncApi2SpecVersion(editorContent) || false;
   const isOAS3_1 = testForOas3_1SpecVersion(editorContent) || false;
-
   return { isOAS3, isOAS3_1, isSwagger2, isAsyncApi2 };
 };
 
 export default { getSpecVersion };
-
-// reference example Swagger UI selectors, which also rely on immutableJS
-// export function isOAS3(jsSpec) {
-//   const oasVersion = jsSpec.get('openapi');
-//   if (typeof oasVersion !== 'string') {
-//     return false;
-//   }
-
-//   // we gate against `3.1` becasue we want to explicitly opt into supporting it
-//   // at some point in the future -- KS, 7/2018
-
-//   // starts with, but is not `3.0.` exactly
-//   return oasVersion.startsWith('3.0.') && oasVersion.length > 4;
-// }
-
-// export function isSwagger2(jsSpec) {
-//   const swaggerVersion = jsSpec.get('swagger');
-//   if (typeof swaggerVersion !== 'string') {
-//     return false;
-//   }
-
-//   return swaggerVersion.startsWith('2.0');
-// }
