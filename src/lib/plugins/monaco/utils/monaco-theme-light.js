d852cacdf498d8affead5d2f359549d7721e4ed1
Tim Lai <timothy.lai@gmail.com>
2021-12-03 13:46:26 -0800

    refactor: updates to light theme with better contrast (#327)

--- src/lib/plugins/monaco/utils/monaco-theme-light.js
+++ src/lib/plugins/monaco/utils/monaco-theme-light.js
@@ -75,59 +75,64 @@ export const themes = {
     base: 'vs', // can also be vs-dark or hc-black
     inherit: true, // can also be false to completely replace the builtin rules
     rules: [
-      { token: 'info', foreground: '#1de2fa', fontStyle: 'bold' },
-      { token: 'version', foreground: '#1de2fa' },
-      { token: 'spec-version', foreground: '#1de2fa' },
-      { token: 'api-version', foreground: '#1de2fa' },
-      { token: 'title', foreground: '#1de2fa' },
-      { token: 'openapi', foreground: '#1de2fa', fontStyle: 'bold' },
-      { token: 'specVersion', foreground: '#1de2fa' },
-      { token: 'asyncApiVersion', foreground: '#1de2fa' },
-
-      { token: 'components', foreground: '#d0d0e3', fontStyle: 'bold' },
-      { token: 'components-parameters', foreground: '#d0d0e3', fontStyle: 'bold' },
-      { token: 'components-schemas', foreground: '#d0d0e3', fontStyle: 'bold' },
-      { token: 'components-messages', foreground: '#d0d0e3', fontStyle: 'bold' },
-      { token: 'parameters', foreground: '#d0d0e3', fontStyle: 'bold' },
-      { token: 'paths', foreground: '#d0d0e3', fontStyle: 'bold' },
-      { token: 'responses', foreground: '#d0d0e3', fontStyle: 'bold' },
-      { token: 'servers', foreground: '#d0d0e3', fontStyle: 'bold' },
-      { token: 'channels', foreground: '#d0d0e3', fontStyle: 'bold' },
-
-      { token: 'openapi-reference', foreground: '#ff2328', fontStyle: 'bold' },
-      { token: 'reference', foreground: '#ff2328', fontStyle: 'bold' },
-      { token: 'Asyncapi-reference', foreground: '#ff2328', fontStyle: 'bold' },
-      { token: 'json-reference', foreground: '#ff2328', fontStyle: 'bold' },
-      { token: 'reference-element', foreground: '#ff2328', fontStyle: 'bold' },
-
-      { token: 'reference-value', foreground: '#ff2328', fontStyle: 'italic' },
-
-      { token: 'server-url', foreground: '#ceca84' },
-      { token: 'content', foreground: '#ceca84' },
-      { token: 'mediaType', foreground: '#ceca84' },
-
-      { token: 'requestBody', foreground: '#ceca84' },
-      { token: 'response', foreground: '#ceca84' },
-      { token: 'server', foreground: '#ceca84' },
-
-      { token: 'parameter', foreground: '#ceca84' },
-
-      { token: 'schema', foreground: '#ceca84', fontStyle: 'bold' },
-
-      { token: 'channelItem', foreground: '#ceca84' },
-
-      { token: 'key.string', foreground: '#e8b9bb', fontStyle: 'bold' },
-      { token: 'key.number', foreground: '#e8b9bb', fontStyle: 'bold' },
-
-      { token: 'pathItem', foreground: '#66afce', fontStyle: 'italic' }, // light blue
-
-      { token: 'operation', foreground: '#66afce', fontStyle: 'bold' },
-      { token: 'operation.httpMethod-POST', foreground: '#C678DD', fontStyle: 'bold' },
-      { token: 'operation.httpMethod-GET', foreground: '#D19A66', fontStyle: 'bold' },
-
-      { token: 'value', foreground: '#98C379' }, // green
-      { token: 'value.string', foreground: '#98C379' }, // green
-      { token: 'value.number', foreground: '#98C379', fontStyle: 'bold' }, // green
+      // top-level tokens: bold dark grey with purple tint
+      { token: 'info', foreground: '#000033', fontStyle: 'bold' },
+      { token: 'spec-version', foreground: '#000033', fontStyle: 'bold' },
+      { token: 'servers', foreground: '#000033', fontStyle: 'bold' },
+      { token: 'paths', foreground: '#000033', fontStyle: 'bold' },
+      { token: 'components', foreground: '#000033', fontStyle: 'bold' },
+      { token: 'channels', foreground: '#000033', fontStyle: 'bold' },
+      // operation tokens: swagger-ui-post=green, swagger-ui-get=blue
+      { token: 'operation', foreground: '#66afce', fontStyle: 'bold' }, // light blue
+      { token: 'operation.httpMethod-GET', foreground: '#006699', fontStyle: 'bold' }, // blue-grey
+      { token: 'operation.httpMethod-POST', foreground: '#339966', fontStyle: 'bold' }, // dark teal
+      // parameters tokens: purple
+      { token: 'parameters', foreground: '#993399', fontStyle: 'italic' },
+      { token: 'parameter', foreground: '#993399', fontStyle: 'italic' },
+      { token: 'components-parameters', foreground: '#993399', fontStyle: 'bold' }, // bug, not rendering as bold
+      // other tokens with purple
+      { token: 'components-messages', foreground: '#993399', fontStyle: 'bold' },
+      // reference & $refs tokens: orange
+      { token: 'reference-element', foreground: '#cc3300', fontStyle: 'bold' },
+      { token: 'reference-value', foreground: '#ff5500', fontStyle: 'italic' },
+      // components/{schemas}/schema: bold olive green
+      { token: 'components-schemas', foreground: '#666633', fontStyle: 'bold' },
+      { token: 'schema', foreground: '#666633', fontStyle: 'bold' },
+      // pathItem & channelItem: bold olive green
+      { token: 'pathItem', foreground: '#666633', fontStyle: 'bold' },
+      { token: 'channelItem', foreground: '#666633', fontStyle: 'bold' },
+      // request & response: olive green
+      { token: 'requestBody', foreground: '#666633', fontStyle: 'italic' },
+      { token: 'responses', foreground: '#666633', fontStyle: 'italic' },
+      { token: 'content', foreground: '#666633' },
+      { token: 'mediaType', foreground: '#666633' },
+      { token: 'response', foreground: '#666633' },
+      { token: 'server', foreground: '#666633' },
+      // plain value: green
+      { token: 'value', foreground: '#339933' },
+      { token: 'value.string', foreground: '#339933' },
+      { token: 'value.number', foreground: '#339933' },
+      // plain key: pumpkin
+      { token: 'key.string', foreground: '#cc6600' },
+      { token: 'key.number', foreground: '#cc6600' },
+      // tbd misc tokens: sky blue
+      { token: 'api-version', foreground: '#0099cc' }, // version inside of info object
+      { token: 'server-url', foreground: '#0099cc' },
+      // tbd unverified tokens, sky blue
+      { token: 'version', foreground: '#000033' },
+      { token: 'title', foreground: '#000033' },
+      { token: 'openapi', foreground: '#000033', fontStyle: 'bold' },
+      { token: 'specVersion', foreground: '#000033' },
+      { token: 'asyncApiVersion', foreground: '#000033' },
+      // tbd unverified tokens, may be orange-ish to match other reference(s)
+      { token: 'openapi-reference', foreground: '#ff9966' },
+      { token: 'reference', foreground: '#ff9966' },
+      { token: 'Asyncapi-reference', foreground: '#ff9966' },
+      { token: 'json-reference', foreground: '#ff9966' },
+      // NYI, color tbd
+      // { token: 'securitySchemes', foreground: '#ff9966' }, // also components/securitySchemes
+      // { token: 'links', foreground: '#ff9966' }, // also components/links
+      // { token: 'security', foreground: '#ff9966' },
     ],
     colors: {
       'editor.background': '#fdf6e3',
