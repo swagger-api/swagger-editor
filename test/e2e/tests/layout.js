056b5b43c2bd41177a96c8e9ae101ac3d4146e64
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-02-21 12:23:40 +0100

    test: simplify tests

--- test/e2e/tests/layout.js
+++ test/e2e/tests/layout.js
@@ -6,43 +6,30 @@ describe("EditorLayout", () => {
 
     describe("given one or more files are of an unexpected type", () => {
       it("should alert the user that their file(s) were rejected", () => {
-        let dropped = 0
         const alert = cy.stub()
-
-        cy.on("window:alert", (text) => {
-          expect(text).to.equal("Sorry, there was an error processing your file.\nPlease drag and drop exactly one .yaml or .json OpenAPI definition file.")
-        })
         cy.on("window:alert", alert)
-        cy.on("window:alert", () => {
-          // when this closure executes the previous alert have already been closed
-          dropped += 1
-          if (dropped === 1) {
-            cy.get("[data-cy=\"dropzone\"]")
-              .attachFile(["rejected.file.1", "rejected.file.2"], { subjectType: "input" })
-              .then(() => {
-                expect(alert.callCount).to.equal(2)
-              })
-          }
-        })
-
         cy.get(".info", { timeout: 10000 }).should("be.visible")
         cy.get("[data-cy=\"dropzone\"]")
           .attachFile("rejected.file.1", { subjectType: "input" })
+          .attachFile(["rejected.file.1", "rejected.file.2"], { subjectType: "input" })
+          .then(() => {
+            expect(alert.callCount).to.equal(2)
+            expect(alert.getCall(0)).to.be.calledWith("Sorry, there was an error processing your file.\nPlease drag and drop exactly one .yaml or .json OpenAPI definition file.")
+          })
       })
     })
 
     describe("when more than one file of an expected type is dropped", () => {
       it("should alert the user that their file(s) were rejected", () => {
-        const stub = cy.stub()
-        cy.on("window:alert", stub)
-        cy.on("window:alert", () => {
-          expect(stub.calledOnce).to.be.true
-          expect(stub.getCall(0)).to.be.calledWith("Sorry, there was an error processing your file.\nPlease drag and drop exactly one .yaml or .json OpenAPI definition file.")
-        })
-
+        const alert = cy.stub()
+        cy.on("window:alert", alert)
         cy.get(".info", { timeout: 10000 }).should("be.visible")
         cy.get("[data-cy=\"dropzone\"]")
           .attachFile(["rejected.file.1", "rejected.file.2"], { subjectType: "input" })
+          .then(() => {
+            expect(alert.calledOnce).to.be.true
+            expect(alert.getCall(0)).to.be.calledWith("Sorry, there was an error processing your file.\nPlease drag and drop exactly one .yaml or .json OpenAPI definition file.")
+          })
       })
     })
 
