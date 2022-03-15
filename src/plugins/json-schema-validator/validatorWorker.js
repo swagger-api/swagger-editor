920ba176c8072f41c458407ee481ca38e0051edd
Timothy Lai <timothy.lai@gmail.com>
2022-03-15 11:39:26 -0700

    refactor(worker): add comment for webpack@5 vs worker-loader

--- src/plugins/json-schema-validator/validatorWorker.js
+++ src/plugins/json-schema-validator/validatorWorker.js
@@ -1,4 +1,10 @@
-const baseUrl = document.baseURI || location.href
-const validatorWorker = new Worker(new URL("./validator.worker.js", baseUrl))
+// use this for webpack@5 native loader`
+// const metaUrl = import.meta.url // does not work as-is; reference path of THIS file
+// const baseUrl = document.baseURI || location.href
+// const validatorWorker = new Worker(new URL("./validator.worker.js", baseUrl))
+
+// use this version for worker-loader
+import Worker from "./validator.worker"
+const validatorWorker = new Worker()
 
 export { validatorWorker }
