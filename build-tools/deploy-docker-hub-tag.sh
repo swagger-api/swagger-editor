3ca073ab6dc39ee8fdf67d7e184e0315e5090faf
Kumar Keshav <kumarkeshav79@gmail.com>
2022-03-18 00:26:34 +0530

    exit

--- build-tools/deploy-docker-hub-tag.sh
+++ build-tools/deploy-docker-hub-tag.sh
@@ -10,3 +10,21 @@ if [ $DOCKER_HUB_USERNAME ]; then
     docker push $DOCKER_IMAGE_NAME:latest;
   fi;
 fi;
+if [ $DOCKER_HUB_USERNAME ]; then
+  docker login --username=$DOCKER_HUB_USERNAME --password=$DOCKER_HUB_PASSWORD;
+
+  if [ ! -z "$TRAVIS_TAG" ]; then
+    DOCKER_IMAGE_TAG=${TRAVIS_TAG#?};
+    docker build -t $DOCKER_IMAGE_NAME .;
+    docker tag $DOCKER_IMAGE_NAME $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG;
+    docker push $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG;
+    docker 
+
+
+
+fi{
+
+  fi
+  exit();
+  
+}
