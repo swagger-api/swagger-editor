7ea4f386f45ff03c99608e33ad5dcd51cf24a8a4
Tim Lai <timothy.lai@gmail.com>
2022-04-29 15:54:10 -0700

    test(validationPane): cypress e2e for visibility (#693)

---
+++ test/cypress/integration/plugin.validation-pane.spec.js
@@ -0,0 +1,40 @@
+describe('Monaco Editor with Validation Pane', () => {
+  beforeEach(() => {
+    cy.prepareAsyncAPI();
+    const moveToPosition = `{rightArrow}{rightArrow}`;
+    // introduce a typo error
+    cy.get('.monaco-editor textarea:first').click().focused().type(`${moveToPosition}Q`);
+  });
+
+  /**
+   * note: future UX may make the table header <thead> always visible,
+   * or existing but collapsed
+   * expect table body <tbody> to always not exist if there are no errors
+   * make appropriate changes if/when needed
+   */
+  it('should display visible Validation Pane table header and table body when error exists', () => {
+    cy.get('.swagger-ide__validation-table > thead').should('exist').should('be.visible');
+    cy.get('.swagger-ide__validation-table > tbody').should('exist').should('be.visible');
+    // some additional assertions just to make sure
+    cy.get('.swagger-ide__validation-table > thead > tr > :nth-child(1)')
+      .contains('line', { matchCase: false })
+      .should('be.visible');
+    cy.get('.swagger-ide__validation-table > thead > tr > :nth-child(2)')
+      .contains('description', { matchCase: false })
+      .should('be.visible');
+    cy.get('.swagger-ide__validation-table > tbody > :nth-child(1) > :nth-child(1) > div')
+      .contains('1')
+      .should('be.visible');
+    cy.get('.swagger-ide__validation-table > tbody > :nth-child(1) > :nth-child(2) > div')
+      .contains('should always have')
+      .should('be.visible');
+  });
+
+  it('should not display Validation Pane after error is cleared', () => {
+    // fix the typo error
+    cy.get('.monaco-editor textarea:first').click().focused().type(`{backspace}`);
+    // re-assert
+    cy.get('.swagger-ide__validation-table > thead').should('not.exist');
+    cy.get('.swagger-ide__validation-table > tbody').should('not.exist');
+  });
+});
