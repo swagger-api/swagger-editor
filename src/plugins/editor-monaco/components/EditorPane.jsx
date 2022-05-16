03eb43c4b61dc8be6dd31f5946f3d096999ed72e
Timothy Lai <timothy.lai@gmail.com>
2022-05-16 13:23:51 -0700

    feat: add resize-detector for monaco EditorPane

--- src/plugins/editor-monaco/components/EditorPane.jsx
+++ src/plugins/editor-monaco/components/EditorPane.jsx
@@ -1,20 +1,35 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { useResizeDetector } from 'react-resize-detector';
 
 const EditorPane = ({ getComponent }) => {
-  const [editorWidth, setEditorWidth] = useState('50');
+  const editorRef = useRef();
+  const [editorWidth, setEditorWidth] = useState('50'); // 50%
   const [editorHeight, setEditorHeight] = useState('80vh');
+  const [containerWidth, setContainerWidth] = useState('50%');
 
-  const onResize = useCallback((width, height) => {
+  const onResizeEditor = useCallback((width, height) => {
     setEditorHeight(height);
     setEditorWidth(width);
   }, []);
 
-  const { ref } = useResizeDetector({
+  useResizeDetector({
     refreshMode: 'debounce',
     refreshRate: 100,
-    onResize,
+    onResize: onResizeEditor,
+    targetRef: editorRef, // custom ref
+  });
+
+  const onResizeContainer = useCallback((width) => {
+    setContainerWidth(width);
+  }, []);
+
+  // as-is, containerRef cannot use the API.targetRef like editorRef
+  const { containerRef } = useResizeDetector({
+    handleHeight: false,
+    refreshMode: 'debounce',
+    refreshRate: 100,
+    onResize: onResizeContainer,
   });
 
   const Editor = getComponent('Editor', true);
@@ -22,9 +37,13 @@ const EditorPane = ({ getComponent }) => {
 
   return (
     <div className="swagger-ide__editor-pane">
-      <div className="swagger-ide__editor-pane-container-col">
+      <div
+        ref={containerRef}
+        style={{ width: containerWidth }}
+        className="swagger-ide__editor-pane-container-col"
+      >
         <EditorPaneBarTop />
-        <div ref={ref} className="swagger-ide__editor-pane-container-row">
+        <div ref={editorRef} className="swagger-ide__editor-pane-container-row">
           <Editor width={editorWidth} height={editorHeight} />
         </div>
       </div>
