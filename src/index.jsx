c78967272d30604e893f86781991a4fc12d5c008
Tim Lai <timothy.lai@smartbear.com>
2022-06-30 14:36:50 -0700

    feat: custom env for SWAGGER_URL and SWAGGER_FILE (#3200)
    * feat: custom env for SWAGGER_URL and SWAGGER_FILE
    
    * docs: add custom env section

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,21 @@ import 'swagger-ui-react/swagger-ui.css';
 
 import SwaggerEditor from './App.jsx';
 
-ReactDOM.render(
-  <SwaggerEditor url="https://raw.githubusercontent.com/asyncapi/spec/v2.4.0/examples/streetlights-kafka.yml" />,
-  document.getElementById('swagger-editor')
-);
+const defaultUrlAsyncApi =
+  'https://raw.githubusercontent.com/asyncapi/spec/v2.4.0/examples/streetlights-kafka.yml';
+// const defaultUrlOas3 = 'https://petstore3.swagger.io/api/v3/openapi.json';
+
+let initialUrl;
+
+if (process.env.REACT_APP_SWAGGER_URL) {
+  // remote url
+  initialUrl = `${process.env.PUBLIC_URL}${process.env.REACT_APP_SWAGGER_URL}`;
+} else if (process.env.REACT_APP_SWAGGER_FILE) {
+  // local file within `{root}/public`
+  // env flag also should include `/static` directory, e.g. `/static/petstore-oas3.yml`
+  initialUrl = `${process.env.PUBLIC_URL}${process.env.REACT_APP_SWAGGER_FILE}`;
+} else {
+  initialUrl = defaultUrlAsyncApi;
+}
+
+ReactDOM.render(<SwaggerEditor url={initialUrl} />, document.getElementById('swagger-editor'));
