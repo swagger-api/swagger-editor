b6f90ca55ef73824a048127747273745202a45b9
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-07-25 12:05:03 +0200

    test: fix

--- test/cypress/integration/plugin.editor-persistence.spec.js
+++ test/cypress/integration/plugin.editor-persistence.spec.js
@@ -1,17 +1,21 @@
 describe('EditorPersistencePlugin', () => {
   beforeEach(() => {
     cy.visitBlankPage();
-    cy.prepareAsyncAPI();
-    cy.waitForSplashScreen();
   });
 
   it('should load definition with provided url prop', () => {
+    cy.prepareAsyncAPI();
+    cy.waitForSplashScreen();
+
     cy.get('.monaco-editor .view-lines')
       .should('contains.text', 'asyncapi')
       .should('contains.text', '2.4.0');
   });
 
   it('should reload while keeping text change from 2.4.0 to 2.3.0', () => {
+    cy.prepareAsyncAPI();
+    cy.waitForSplashScreen();
+
     const moveToPosition = `{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}`;
 
     cy.get('.monaco-editor textarea:first', { timeout: 10000 })
@@ -20,10 +24,9 @@ describe('EditorPersistencePlugin', () => {
       .focused()
       .type(`${moveToPosition}{shift+rightArrow}3`);
 
-    cy.waitForContentPropagation();
-
     cy.get('.monaco-editor .view-lines').should('contains.text', '2.3.0');
 
+    cy.waitForContentPropagation();
     cy.reload(true);
 
     cy.waitForSplashScreen();
