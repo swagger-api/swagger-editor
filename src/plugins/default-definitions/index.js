62e9df1b3a0116ea3c822a459ac919add9c6c46a
Frank Kilcommins <frank.kilcommins@smartbear.com>
2022-07-26 18:06:34 +0100

    Merged patch from Vladimir for cy:ci e2e tests

--- src/plugins/default-definitions/index.js
+++ src/plugins/default-definitions/index.js
@@ -3,7 +3,7 @@ info:
   title: Swagger Petstore - OpenAPI 3.0
   description: |-
     This is a sample Pet Store Server based on the OpenAPI 3.0 specification.  You can find out more about
-    Swagger at [http://swagger.io](http://swagger.io). In the third iteration of the pet store, we've switched to the design first approach!
+    Swagger at [https://swagger.io](https://swagger.io). In the third iteration of the pet store, we've switched to the design first approach!
     You can now help us improve the API whether it's by making changes to the definition itself or to the code.
     That way, with time, we can improve the API in general, and expose some of the new features in OAS3.
 
