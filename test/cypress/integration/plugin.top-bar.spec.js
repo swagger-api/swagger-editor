3f1776394df95aa3a4752864f301305c3a302b8f
Tim Lai <timothy.lai@smartbear.com>
2022-07-29 14:15:45 -0700

    fix: `contentType` value returned by oas30x, oas31x YAMLMatch (#3322)
    * fix: contentType returned by oas30x, oas31x YAMLMatch
    
    * test(topbar): contentType validation for json/yaml cases

--- test/cypress/integration/plugin.top-bar.spec.js
+++ test/cypress/integration/plugin.top-bar.spec.js
@@ -29,28 +29,110 @@ describe('Topbar', () => {
       cy.contains('Clear').trigger('mousemove').click();
       cy.get('.view-lines > :nth-child(1)').should('to.have.text', '');
     });
-    it('should be able to Load a fixture as YAML', () => {
+
+    describe('should be able to Load a fixture as YAML', () => {
       /**
        * Fixtures are JSON
        * YAML option is progammatically set per menu item
        */
-      cy.contains('Edit').click();
-      cy.contains('Load OpenAPI 3.0 Fixture').trigger('mousemove').click();
-      cy.get('.view-lines > :nth-child(1)')
-        .should('contains.text', 'openapi')
-        .should('not.have.text', '{')
-        .should('not.have.text', '"');
+      it('loads OpenAPI 3.0 Fixture as YAML', () => {
+        cy.contains('Edit').click();
+        cy.contains('Load OpenAPI 3.0 Fixture').trigger('mousemove').click();
+        cy.get('.view-lines > :nth-child(1)')
+          .should('contains.text', 'openapi')
+          .should('not.have.text', '{')
+          .should('not.have.text', '"');
+      });
+      it('loads OpenAPI 3.1 Fixture as YAML', () => {
+        cy.contains('Edit').click();
+        cy.contains('Load OpenAPI 3.1 Fixture').trigger('mousemove').click();
+        cy.get('.view-lines > :nth-child(1)')
+          .should('contains.text', 'openapi')
+          .should('not.have.text', '{')
+          .should('not.have.text', '"');
+      });
+      it('loads OpenAPI 2.0 Fixture as YAML', () => {
+        cy.contains('Edit').click();
+        cy.contains('Load OpenAPI 2.0 Fixture').trigger('mousemove').click();
+        cy.get('.view-lines > :nth-child(1)')
+          .should('contains.text', 'swagger')
+          .should('not.have.text', '{')
+          .should('not.have.text', '"');
+      });
+      it('loads AsyncAPI 2.4 Fixture as YAML', () => {
+        cy.contains('Edit').click();
+        cy.contains('Load AsyncAPI 2.4 Fixture').trigger('mousemove').click();
+        cy.get('.view-lines > :nth-child(1)')
+          .should('contains.text', 'asyncapi')
+          .should('not.have.text', '{')
+          .should('not.have.text', '"');
+      });
     });
-    it('should be able to Load a fixture as JSON', () => {
+
+    describe('should be able to Load a fixture as JSON', () => {
       /**
        * Final production version might not contain
-       * a fixture that we intend to load as JSON.
-       * If so, please disable this test
+       * a fixture that we intend to load and display as JSON.
+       * If so, please disable these test(s)
        */
-      cy.contains('Edit').click();
-      cy.contains('Load AsyncAPI 2.4 Petstore Fixture').trigger('mousemove').click();
-      cy.get('.view-lines > :nth-child(1)').should('contains.text', '{');
-      cy.get('.view-lines > :nth-child(2)').should('contains.text', '"asyncapi"'); // note the double quotes
+      it('loads AsyncAPI 2.4 Petstore Fixture as JSON', () => {
+        cy.contains('Edit').click();
+        cy.contains('Load AsyncAPI 2.4 Petstore Fixture').trigger('mousemove').click();
+        cy.get('.view-lines > :nth-child(1)').should('contains.text', '{');
+        cy.get('.view-lines > :nth-child(2)').should('contains.text', '"asyncapi"'); // note the double quotes
+      });
+      it('loads API Design Systems Fixture as JSON', () => {
+        cy.contains('Edit').click();
+        cy.contains('Load API Design Systems Fixture').trigger('mousemove').click();
+        cy.get('.view-lines > :nth-child(1)').should('contains.text', '{');
+      });
+    });
+
+    describe('should display "Convert To JSON" menu item after loading fixture as YAML', () => {
+      it('displays "Convert To JSON" option for OpenAPI 3.0 Fixture', () => {
+        cy.contains('Edit').click();
+        cy.contains('Load OpenAPI 3.0 Fixture').trigger('mousemove').click();
+        cy.contains('Edit').click();
+        cy.contains('Convert to JSON').should('be.visible');
+      });
+      it('displays "Convert To JSON" option for OpenAPI 3.1 Fixture', () => {
+        cy.contains('Edit').click();
+        cy.contains('Load OpenAPI 3.1 Fixture').trigger('mousemove').click();
+        cy.contains('Edit').click();
+        cy.contains('Convert to JSON').should('be.visible');
+      });
+      it('displays "Convert To JSON" option for OpenAPI 2.0 Fixture', () => {
+        cy.contains('Edit').click();
+        cy.contains('Load OpenAPI 2.0 Fixture').trigger('mousemove').click();
+        cy.contains('Edit').click();
+        cy.contains('Convert to JSON').should('be.visible');
+      });
+      it('displays "Convert To JSON" option for AsyncAPI 2.4 Fixture', () => {
+        cy.contains('Edit').click();
+        cy.contains('Load AsyncAPI 2.4 Fixture').trigger('mousemove').click();
+        cy.contains('Edit').click();
+        cy.contains('Convert to JSON').should('be.visible');
+      });
+    });
+
+    describe('should display "Convert to YAML" menu item after loading fixture as JSON', () => {
+      /**
+       * Final production version might not contain
+       * a fixture that we intend to load and display as JSON.
+       * If so, please disable these tests
+       */
+      it('displays "Convert To YAML" option for AsyncAPI 2.4 Petstore Fixture', () => {
+        cy.contains('Edit').click();
+        cy.contains('Load AsyncAPI 2.4 Petstore Fixture').trigger('mousemove').click();
+        cy.contains('Edit').click();
+        cy.contains('Convert to YAML').should('be.visible');
+      });
+      it('displays "Convert To YAML" option for API Design Systems Fixture', () => {
+        cy.contains('Edit').click();
+        cy.contains('Load API Design Systems Fixture').trigger('mousemove').click();
+        cy.contains('Edit').click();
+        cy.contains('Convert to YAML').should('be.visible');
+      });
     });
   });
 });
