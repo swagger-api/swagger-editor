f5e6b73f3871291d90ccbb8c8ca6a7615aac0cbc
Timothy Lai <timothy.lai@smartbear.com>
2022-08-05 11:00:08 -0700

    feat(test): e2e theme toggle

--- test/cypress/integration/plugin.editor-monaco.spec.js
+++ test/cypress/integration/plugin.editor-monaco.spec.js
@@ -38,4 +38,24 @@ describe('Monaco Editor with Parser', () => {
     cy.get('@consoleError').should('not.be.called');
     cy.get('.monaco-editor .view-lines').should('contains.text', 'randomapi');
   });
+
+  it('should toggle between light and dark themes', () => {
+    // default dark
+    cy.get(':nth-child(2) > .swagger-editor__editor-pane-bar-control > .octicon-sun').should(
+      'be.visible'
+    );
+    cy.get('.swagger-editor__editor-monaco > .vs-dark ').should('exist');
+    // toggle to light
+    cy.get(':nth-child(2) > .swagger-editor__editor-pane-bar-control').click();
+    cy.get(':nth-child(2) > .swagger-editor__editor-pane-bar-control > .octicon-moon').should(
+      'be.visible'
+    );
+    cy.get('.swagger-editor__editor-monaco > .vs ').should('exist');
+    // toggle back to dark
+    cy.get(':nth-child(2) > .swagger-editor__editor-pane-bar-control').click();
+    cy.get(':nth-child(2) > .swagger-editor__editor-pane-bar-control > .octicon-sun').should(
+      'be.visible'
+    );
+    cy.get('.swagger-editor__editor-monaco > .vs-dark ').should('exist');
+  });
 });
