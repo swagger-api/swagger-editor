0008d75a9d7a206c15d7be6fe81ad78be9e97e58
Tim Lai <timothy.lai@smartbear.com>
2022-08-05 11:17:24 -0700

    feat(test): e2e for toggle readonly and toggle theme (#3356)
    * feat(test): e2e theme toggle
    
    * feat(test): e2e read-only toggle

---
+++ test/cypress/integration/plugin.editor-read-only.spec.js
@@ -0,0 +1,32 @@
+describe('read only', () => {
+  beforeEach(() => {
+    cy.visitBlankPage();
+    cy.prepareAsyncAPI();
+    cy.waitForSplashScreen();
+  });
+  it('should toggle between allow-write and read-only', () => {
+    // default allow-write
+    cy.get(':nth-child(1) > .swagger-editor__editor-pane-bar-control > .octicon-unlock').should(
+      'be.visible'
+    );
+    cy.get('.monaco-editor textarea:first', { timeout: 10000 })
+      .should('be.visible')
+      .should('not.have.attr', 'readonly');
+    // toggle to read-only
+    cy.get(':nth-child(1) > .swagger-editor__editor-pane-bar-control').click();
+    cy.get(':nth-child(1) > .swagger-editor__editor-pane-bar-control > .octicon-lock').should(
+      'be.visible'
+    );
+    cy.get('.monaco-editor textarea:first', { timeout: 10000 })
+      .should('be.visible')
+      .should('have.attr', 'readonly');
+    // toggle back to allow-write
+    cy.get(':nth-child(1) > .swagger-editor__editor-pane-bar-control').click();
+    cy.get(':nth-child(1) > .swagger-editor__editor-pane-bar-control > .octicon-unlock').should(
+      'be.visible'
+    );
+    cy.get('.monaco-editor textarea:first', { timeout: 10000 })
+      .should('be.visible')
+      .should('not.have.attr', 'readonly');
+  });
+});
