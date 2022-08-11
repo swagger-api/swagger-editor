9007900189f6dda852fa96c9379544987f4ce576
Timothy Lai <timothy.lai@smartbear.com>
2022-08-11 10:04:46 -0700

    test(e2e): try fix for flaky editor-persistence

--- test/cypress/integration/plugin.editor-persistence.spec.js
+++ test/cypress/integration/plugin.editor-persistence.spec.js
@@ -8,24 +8,24 @@ describe('EditorPersistencePlugin', () => {
     cy.prepareAsyncAPI();
     cy.waitForSplashScreen();
 
-    cy.get('.monaco-editor .view-lines')
+    cy.get('.monaco-editor .view-lines > :nth-child(1)')
       .should('contains.text', 'asyncapi')
       .should('contains.text', '2.4.0');
   });
 
-  it.skip('should reload while keeping text change from 2.4.0 to 2.3.0', () => {
+  it('should reload while keeping text change from 2.4.0 to 2.3.0', () => {
     cy.prepareAsyncAPI();
     cy.waitForSplashScreen();
 
     const moveToPosition = `{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}{rightArrow}`;
 
-    cy.get('.monaco-editor textarea:first', { timeout: 10000 })
+    cy.get('.monaco-editor textarea:first')
       .should('be.visible')
       .click({ force: true })
       .focused()
       .type(`${moveToPosition}{shift+rightArrow}3`);
 
-    cy.get('.monaco-editor .view-lines')
+    cy.get('.monaco-editor .view-lines > :nth-child(1)')
       .should('contains.text', '2.3.0')
       .should('not.contains.text', '2.4.0');
 
@@ -33,7 +33,7 @@ describe('EditorPersistencePlugin', () => {
     cy.reload();
 
     cy.waitForSplashScreen();
-    cy.get('.monaco-editor .view-lines')
+    cy.get('.monaco-editor .view-lines > :nth-child(1)')
       .should('contains.text', '2.3.0')
       .should('not.contains.text', '2.4.0');
   });
