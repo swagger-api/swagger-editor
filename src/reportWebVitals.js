793dd8505e97855a76b7404f03a23f3b3b665d95
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-09-01 12:33:32 +0200

    refactor(reportWebVitals): adapt to new API
    Refs #3419

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,12 +1,12 @@
 const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     // eslint-disable-next-line import/no-extraneous-dependencies
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+    import('web-vitals').then(({ onCLS, onFID, onFCP, onLCP, onTTFB }) => {
+      onCLS(onPerfEntry);
+      onFID(onPerfEntry);
+      onFCP(onPerfEntry);
+      onLCP(onPerfEntry);
+      onTTFB(onPerfEntry);
     });
   }
 };
