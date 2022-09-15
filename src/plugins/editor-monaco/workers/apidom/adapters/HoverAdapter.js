71ff9573c27fae8ff4053af496dc6677c9783aac
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-09-15 14:45:47 +0200

    refactor: adapt hover adapter

--- src/plugins/editor-monaco/workers/apidom/adapters/HoverAdapter.js
+++ src/plugins/editor-monaco/workers/apidom/adapters/HoverAdapter.js
@@ -1,11 +1,12 @@
+import { createConverter as createCodeConverter } from 'vscode-languageclient/lib/common/codeConverter.js';
 import { createConverter as createProtocolConverter } from 'vscode-languageclient/lib/common/protocolConverter.js';
-
-import { fromPosition } from './monaco-helpers.js';
 
 export default class HoverAdapter {
   #worker;
 
-  #p2m = createProtocolConverter(undefined, true, true);
+  #codeConverter = createCodeConverter();
+
+  #protocolConverter = createProtocolConverter(undefined, true, true);
 
   constructor(worker) {
     this.#worker = worker;
@@ -15,22 +16,15 @@ export default class HoverAdapter {
     const worker = await this.#worker(model.uri);
 
     try {
-      const computedPosition = fromPosition(position);
-      const hover = await worker.doHover(model.uri.toString(), computedPosition);
-
-      return hover ?? null;
+      return await worker.doHover(model.uri.toString(), this.#codeConverter.asPosition(position));
     } catch {
-      return null;
+      return undefined;
     }
-  }
-
-  #maybeConvert(hover) {
-    return hover === null ? null : this.#p2m.asHover(hover);
   }
 
   async provideHover(model, position) {
     const hover = await this.#getHover(model, position);
 
-    return this.#maybeConvert(hover);
+    return this.#protocolConverter.asHover(hover);
   }
 }
