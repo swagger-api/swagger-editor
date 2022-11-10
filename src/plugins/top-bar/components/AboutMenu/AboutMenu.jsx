bacfd8274174627af4d35abdeaaf4dcc0746bb0f
Frank Kilcommins <frank.kilcommins@smartbear.com>
2022-11-10 15:20:20 +0000

    PR review fixes for styling and fragment clean up

--- src/plugins/top-bar/components/AboutMenu/AboutMenu.jsx
+++ src/plugins/top-bar/components/AboutMenu/AboutMenu.jsx
@@ -5,29 +5,23 @@ const AboutMenu = ({ getComponent }) => {
   const Link = getComponent('Link');
 
   return (
-    <>
-      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      <DropdownMenu label="About">
-        <li>
-          <Link href="https://swagger.io/tools/swagger-editor/" target="_blank">
-            About Swagger Editor
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="https://swagger.io/docs/open-source-tools/swagger-editor-next/"
-            target="_blank"
-          >
-            View Docs
-          </Link>
-        </li>
-        <li>
-          <Link href="https://github.com/swagger-api/swagger-editor/tree/next" target="_blank">
-            View on GitHub
-          </Link>
-        </li>
-      </DropdownMenu>
-    </>
+    <DropdownMenu label="About">
+      <li className="dropdown-item">
+        <Link href="https://swagger.io/tools/swagger-editor/" target="_blank">
+          About Swagger Editor
+        </Link>
+      </li>
+      <li className="dropdown-item">
+        <Link href="https://swagger.io/docs/open-source-tools/swagger-editor-next/" target="_blank">
+          View Docs
+        </Link>
+      </li>
+      <li className="dropdown-item">
+        <Link href="https://github.com/swagger-api/swagger-editor/tree/next" target="_blank">
+          View on GitHub
+        </Link>
+      </li>
+    </DropdownMenu>
   );
 };
 
