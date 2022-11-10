3f296423e20d4033e7f596cbc465e9b381821434
Vladimír Gorej <vladimir.gorej@gmail.com>
2022-11-10 17:54:33 +0100

    Update AboutMenu.jsx

--- src/plugins/top-bar/components/AboutMenu/AboutMenu.jsx
+++ src/plugins/top-bar/components/AboutMenu/AboutMenu.jsx
@@ -6,21 +6,27 @@ const AboutMenu = ({ getComponent }) => {
 
   return (
     <DropdownMenu label="About">
-      <li className="dropdown-item">
-        <Link href="https://swagger.io/tools/swagger-editor/" target="_blank">
-          About Swagger Editor
-        </Link>
-      </li>
-      <li className="dropdown-item">
-        <Link href="https://swagger.io/docs/open-source-tools/swagger-editor-next/" target="_blank">
-          View Docs
-        </Link>
-      </li>
-      <li className="dropdown-item">
-        <Link href="https://github.com/swagger-api/swagger-editor/tree/next" target="_blank">
-          View on GitHub
-        </Link>
-      </li>
+      <div> 
+        <li className="dropdown-item">
+          <Link href="https://swagger.io/tools/swagger-editor/" target="_blank">
+            About Swagger Editor
+          </Link>
+        </li>
+      </div>
+      <div>
+        <li className="dropdown-item">
+          <Link href="https://swagger.io/docs/open-source-tools/swagger-editor-next/" target="_blank">
+            View Docs
+          </Link>
+        </li>
+      </div>
+      <div>
+        <li className="dropdown-item">
+          <Link href="https://github.com/swagger-api/swagger-editor/tree/next" target="_blank">
+            View on GitHub
+          </Link>
+        </li>
+      </div>  
     </DropdownMenu>
   );
 };
