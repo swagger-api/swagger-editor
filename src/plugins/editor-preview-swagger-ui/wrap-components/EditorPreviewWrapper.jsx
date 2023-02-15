f16e1f041e435de2cf976a3ac3e9503ec656b128
Timothy Lai <timothy.lai@smartbear.com>
2023-02-15 14:33:51 -0800

    feat(oas3): integrate swagger-ui containing openapi3.1 support

--- src/plugins/editor-preview-swagger-ui/wrap-components/EditorPreviewWrapper.jsx
+++ src/plugins/editor-preview-swagger-ui/wrap-components/EditorPreviewWrapper.jsx
@@ -4,14 +4,17 @@ import PropTypes from 'prop-types';
 const EditorPreviewWrapper = (Original, system) => {
   const EditorPreview = ({ getComponent, editorSelectors }) => {
     const EditorPreviewSwaggerUI = getComponent('EditorPreviewSwaggerUI', true);
-    const EditorPreviewSwaggerUIFallback = getComponent('EditorPreviewSwaggerUIFallback', true);
+    /**
+     * Render `EditorPreviewSwaggerUIFallback` if SwaggerUI does not support targeted OpenAPI version
+     */
+    // const EditorPreviewSwaggerUIFallback = getComponent('EditorPreviewSwaggerUIFallback', true);
     const isOpenAPI = editorSelectors.selectIsContentTypeOpenAPI();
     const isOpenAPI31 = editorSelectors.selectIsContentTypeOpenAPI31x();
     if (isOpenAPI && !isOpenAPI31) {
       return <EditorPreviewSwaggerUI />;
     }
     if (isOpenAPI && isOpenAPI31) {
-      return <EditorPreviewSwaggerUIFallback />;
+      return <EditorPreviewSwaggerUI />;
     }
     return (
       <Original {...system} /> // eslint-disable-line react/jsx-props-no-spreading
