e1306d4d341413963bfe491db966fe3cb3a322ae
Vladimír Gorej <vladimir.gorej@gmail.com>
2023-03-31 15:07:10 +0200

    refactor(language-apidom): enhance dereference monaco action (#4000)
    - infere format (JSON/YAML) from editor content
    - do nothing on unknown format
    - use js-yaml library to pretty-format the dereferenced YAML

--- src/plugins/editor-monaco-language-apidom/language/actions/dereference.js
+++ src/plugins/editor-monaco-language-apidom/language/actions/dereference.js
@@ -1,17 +1,31 @@
+import YAML from 'js-yaml';
+
 import { getWorker } from '../apidom-mode.js';
 
-const dereferenceActionDescriptor = {
+const createDereferenceActionDescriptor = ({ getSystem }) => ({
   id: 'swagger.editor.apidomDereference',
   label: 'Resolve document',
   async run(editor) {
+    const system = getSystem();
+    const isContentJSON = system.editorSelectors.selectIsContentFormatJSON();
+    const isContentYAML = system.editorSelectors.selectIsContentFormatYAML();
+
+    if (!isContentJSON && !isContentYAML) return; // nothing to do here
+
     const model = editor.getModel();
     const worker = await getWorker()(model.uri);
     const dereferenced = await worker.doDeref(model.uri.toString(), {
       baseURI: globalThis.document.baseURI || globalThis.location.href,
+      format: isContentJSON ? 0 : isContentYAML ? 1 : 'unknown', // eslint-disable-line no-nested-ternary
     });
 
-    editor.setValue(dereferenced);
+    if (isContentYAML) {
+      const nicelyFormattedYAML = YAML.dump(YAML.load(dereferenced));
+      editor.setValue(nicelyFormattedYAML);
+    } else if (isContentJSON) {
+      editor.setValue(dereferenced);
+    }
   },
-};
+});
 
-export default dereferenceActionDescriptor;
+export default createDereferenceActionDescriptor;
