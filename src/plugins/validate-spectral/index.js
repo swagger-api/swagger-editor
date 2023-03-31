d15bfe8ddabc149e1d54ebf4f7c1d7ecb525a0dd
Florian Ellinger <florian.ellinger@huk-coburg.de>
2023-03-31 09:31:30 +0200

    fix line offset & severity errors

--- src/plugins/validate-spectral/index.js
+++ src/plugins/validate-spectral/index.js
@@ -10,6 +10,14 @@ export const updateJsonSpec = (ori, { specActions }) => (...args) => {
 
 //eslint-disable-next-line no-unused-vars
 export const validateSpec = (jsSpec) => (arg) => {
+    const severityToLevel = {
+        0: "error",
+        1: "warning",
+        2: "info",
+        3: "info",
+        4: "info",
+    }
+
     // NOTE: This assumes that the REST API is available under the same host
     // TODO: This might need to use a different ruleset depending on input
     fetch(SPECTRAL_HOST + "/valigator/api/validate?ruleset=v5", {
@@ -21,10 +29,10 @@ export const validateSpec = (jsSpec) => (arg) => {
     }).then(response => response.json()).then(response => {
         const errors = response.map((entry) => {
             return {
-                level: "error",
+                level: severityToLevel[entry.severity],
                 message: entry.message,
                 path: entry.path,
-                line: entry.range.start.line
+                line: entry.range.start.line + 1
             }
         })
         if (errors.length > 0) {
