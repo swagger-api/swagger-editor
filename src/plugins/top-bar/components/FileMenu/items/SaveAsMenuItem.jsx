64ebb1f532c3549ddb6cbd376ed0609687e02b8f
Vladimír Gorej <vladimir.gorej@gmail.com>
2023-11-16 13:55:35 +0100

    chore(deps-dev): bump prettier from 3.0.3 to 3.1.0 (#4587)
    Closes #4573

--- src/plugins/top-bar/components/FileMenu/items/SaveAsMenuItem.jsx
+++ src/plugins/top-bar/components/FileMenu/items/SaveAsMenuItem.jsx
@@ -2,12 +2,11 @@ import PropTypes from 'prop-types';
 
 const SaveAsMenuItem = ({ getComponent, editorSelectors, onClick, children }) => {
   const DropdownMenuItem = getComponent('DropdownMenuItem');
-  // eslint-disable-next-line no-nested-ternary
   const itemName = editorSelectors.selectIsContentFormatJSON()
     ? 'Save (as JSON)'
     : editorSelectors.selectIsContentFormatYAML()
-    ? 'Save (as YAML)'
-    : 'Save';
+      ? 'Save (as YAML)'
+      : 'Save';
 
   return <DropdownMenuItem onClick={onClick}>{children || itemName}</DropdownMenuItem>;
 };
