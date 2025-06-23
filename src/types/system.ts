528df78e0559bc59ec96608e732937415d29f665
Robert Hebel <robert.hebel@smartbear.com>
2025-06-23 10:52:31 +0200

    refactor: make types more accurate

--- src/types/system.ts
+++ src/types/system.ts
@@ -1,14 +1,19 @@
 import { PropChangedAction } from 'plugins/props-change-watcher/actions';
 import { Action } from 'types/actions';
+import { ComponentType } from 'react';
 
 export interface SystemValues {
-  getComponent: () => void;
+  getComponent: (
+    componentName: string,
+    container?: boolean,
+    config?: object
+  ) => ComponentType | null;
   editorSelectors: {
     selectContent: () => string;
     selectInferFileNameWithExtensionFromContent: () => string;
   };
   editorActions: {
-    convertContentToJSON: (content: string) => Action<string>;
+    convertContentToJSON: (content: string) => Action<string | Error>;
     setContent: (content: string) => Action<string>;
     propChanged: PropChangedAction;
   };
