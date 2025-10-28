6cb6c3c59c6bd96d37ae930cde54e5cbf8b7cab4
Oliwia Rogala <oliwia.rogala@smartbear.com>
2025-10-28 11:10:44 +0100

    build: remove unnecessary postcss configs

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -82,13 +82,9 @@ export default (webpackEnv) => {
             ident: 'postcss',
             config: false,
             plugins: [
-              'postcss-flexbugs-fixes',
               [
                 'postcss-preset-env',
                 {
-                  autoprefixer: {
-                    flexbox: 'no-2009',
-                  },
                   stage: 3,
                 },
               ],
