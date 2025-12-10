6a775f80eef69b3dd15f49ce5e6169c1d1252113
Oliwia Rogala <oliwia.rogala@smartbear.com>
2025-12-10 15:06:04 +0100

    fix(editor-preview-asyncapi): replace parser function to avoid errors in resolved definitions

---
+++ src/plugins/editor-preview-asyncapi/util/ruleset/async-3/functions.js
@@ -0,0 +1,49 @@
+import { createRulesetFunction } from '@stoplight/spectral-core';
+
+/**
+ * Copy of @asyncapi/parser function which causes issues with resolved specifications.
+ * Replacing the original function with this copy fixes the issue.
+ * https://github.com/asyncapi/parser-js/blob/master/packages/parser/src/ruleset/v3/functions/operationMessagesUnambiguity.ts
+ */
+
+const referenceSchema = {
+  type: 'object',
+  properties: {
+    $ref: {
+      type: 'string',
+      format: 'uri-reference',
+    },
+  },
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export const operationMessagesUnambiguity = createRulesetFunction(
+  {
+    input: {
+      type: 'object',
+      properties: {
+        channel: referenceSchema,
+        messages: {
+          type: 'array',
+          items: referenceSchema,
+        },
+      },
+    },
+    options: null,
+  },
+  (targetVal, _, ctx) => {
+    const results = [];
+    const channelPointer = targetVal.channel?.$ref; // required
+
+    targetVal.messages?.forEach((message, index) => {
+      if (message.$ref && !message.$ref.startsWith(`${channelPointer}/messages`)) {
+        results.push({
+          message: 'Operation message does not belong to the specified channel.',
+          path: [...ctx.path, 'messages', index],
+        });
+      }
+    });
+
+    return results;
+  }
+);
